44516383c6b383fd94678fa2b118138c9d3012de
LeiWang1999 <leiwang1999@outlook.com>
2025-01-24 18:56:59 +0000

    add test for msg

--- testing/python/debug/test_tilelang_debug_print.py
+++ testing/python/debug/test_tilelang_debug_print.py
@@ -81,5 +81,23 @@ def test_debug_print_register_files():
     debug_print_register_files(16, 16)
 
 
+def debug_print_msg(M=16, N=16):
+    dtype = "float16"
+
+    @T.prim_func
+    def program(Q: T.Buffer((M, N), dtype)):
+        with T.Kernel(4, 4, 2, threads=128 * 2) as (bx, by, bz):
+            tid = T.get_thread_binding()
+            if tid == 0:
+                T.print(bx + by + bz, msg="hello world")
+
+    jit_kernel = tilelang.JITKernel(program, target="cuda")
+    profiler = jit_kernel.get_profiler()
+    profiler.run_once()
+
+
+def test_debug_print_msg():
+    debug_print_msg(16, 16)
+
 if __name__ == "__main__":
     tilelang.testing.main()
