c99588e21021453f615ecbf5913c3220113bb1cc
Yu Cheng <yu.cheng@pku.edu.cn>
2025-02-16 05:22:44 -0800

    bug fix

--- examples/flash_decoding/example_mla_decode.py
+++ examples/flash_decoding/example_mla_decode.py
@@ -46,13 +46,17 @@ def flashattn(batch, heads, kv_head_num, seqlen_kv, dim, block_N, block_H):
             sid = bz
             cur_kv_head = hid // (kv_group_num // block_H)
 
+            T.annotate_layout({
+                O_shared: tilelang.layout.make_swizzled_layout(O_shared),
+            })
+
             T.copy(Q[bid, hid * VALID_BLOCK_H:(hid + 1) * VALID_BLOCK_H, :], Q_shared)
             T.fill(acc_o, 0)
             T.fill(logsum, 0)
             T.fill(scores_max, -T.infinity(accum_dtype))
 
             loop_range = T.ceildiv((seqlen_kv // num_split), block_N)
-            for k in T.Pipelined(loop_range, num_stages=2):
+            for k in T.Pipelined(loop_range, num_stages=1):
                 T.copy(
                     K[bid, (seqlen_kv // num_split) * sid +
                       k * block_N:(seqlen_kv // num_split) * sid + (k + 1) * block_N,
@@ -74,8 +78,8 @@ def flashattn(batch, heads, kv_head_num, seqlen_kv, dim, block_N, block_H):
                     acc_o[i, j] *= scores_scale[i]
                 T.copy(
                     V[bid, (seqlen_kv // num_split) * sid +
-                      k * block_N:(seqlen_kv // num_split) * sid + (k + 1) * block_N, hid, :],
-                    V_shared)
+                      k * block_N:(seqlen_kv // num_split) * sid + (k + 1) * block_N,
+                      cur_kv_head, :], V_shared)
                 T.gemm(acc_s_cast, V_shared, acc_o, policy=T.GemmWarpPolicy.FullRow)
             for i, j in T.Parallel(block_H, dim):
                 acc_o[i, j] /= logsum[i]
@@ -110,7 +114,7 @@ def flashattn(batch, heads, kv_head_num, seqlen_kv, dim, block_N, block_H):
             T.clear(o_accum_local)
             for k in T.Parallel(num_split):
                 lse_local[k, 0] = glse[bz, by, k]
-            T.reduce_max(lse_local, lse_max_local, dim=0, clear=False)
+            T.reduce_max(lse_local, lse_max_local, dim=0, clear=True)
             for k in T.Pipelined(num_split, num_stages=1):
                 lse_local_split[0] = glse[bz, by, k]
                 lse_logsum_local[0] += T.exp2(lse_local_split[0] - lse_max_local[0])
@@ -172,6 +176,62 @@ def ref_program(query, key, value, glse, Output_partial):
     return output.view(batch_size, query_heads, dim)
 
 
+def flash_split_ref(Q, K, V):
+    # from einops import rearrange
+    # [batch, seqlen_q, heads, dim]
+    batch = Q.size(0)
+    nheads = Q.size(1)
+    dim = Q.size(2)
+    block_N = 32
+    seqlen_kv = K.size(1)
+
+    scale = (1.0 / dim)**0.5 * 1.44269504  # log2(e)
+    acc_s = torch.empty((batch, nheads, block_N), device="cuda", dtype=torch.float)
+    acc_s_cast = torch.empty((batch, nheads, block_N), device="cuda", dtype=torch.float16)
+    acc_o = torch.empty((batch, nheads, dim), device="cuda", dtype=torch.float)
+    scores_max = torch.empty((batch, nheads), device="cuda", dtype=torch.float)
+    scores_max_prev = torch.empty((batch, nheads), device="cuda", dtype=torch.float)
+    scores_scale = torch.empty((batch, nheads), device="cuda", dtype=torch.float)
+    scores_sum = torch.empty((batch, nheads), device="cuda", dtype=torch.float)
+    logsum = torch.empty((batch, nheads), device="cuda", dtype=torch.float)
+    gacc_o = torch.empty((num_split, batch, nheads, dim), device="cuda", dtype=torch.float)
+    glogsum = torch.empty((num_split, batch, nheads), device="cuda", dtype=torch.float)
+
+    Q_ = Q * scale
+    K_ = K.expand(-1, -1, nheads, -1)
+    V_ = V.expand(-1, -1, nheads, -1)
+
+    for ks in range(num_split):
+        acc_o.fill_(0)
+        logsum.fill_(0)
+        scores_max.fill_(float('-inf'))
+        scores_max_prev.fill_(float('-inf'))
+        for i in range(int((seqlen_kv // num_split) / block_N)):
+            acc_s.fill_(0)
+            acc_s = torch.einsum('bhd,bkhd->bhk', Q_,
+                                 K_[:, (seqlen_kv // num_split) * ks +
+                                    i * block_N:(seqlen_kv // num_split) * ks +
+                                    (i + 1) * block_N, :, :])  # [batch, nheads, block_N]
+            scores_max_prev = scores_max
+            scores_max = acc_s.max(dim=-1, keepdim=False).values  # [batch, nheads]
+            scores_scale = torch.exp2(scores_max_prev - scores_max)  # [batch, nheads]
+            acc_o *= scores_scale[:, :, None]
+            acc_s = torch.exp2(acc_s - scores_max[:, :, None])
+            acc_s_cast = acc_s.to(torch.float16)  # [batch, nheads, block_N]
+            acc_o += torch.einsum(
+                'bhk,bkhd->bhd', acc_s_cast,
+                V_[:, (seqlen_kv // num_split) * ks + i * block_N:(seqlen_kv // num_split) * ks +
+                   (i + 1) * block_N, :, :])
+            scores_sum = acc_s.sum(dim=-1, keepdim=False)
+            logsum = logsum * scores_scale + scores_sum
+        acc_o /= logsum[:, :, None]
+        logsum = torch.log2(logsum) + scores_max
+        gacc_o[ks, :, :, :] = acc_o
+        glogsum[ks, :, :] = logsum
+
+    return glogsum.to(torch.float16).permute(1, 2, 0), gacc_o.to(torch.float16).permute(1, 2, 0, 3)
+
+
 def reduce_ref(Q, K, V, glse, Output_partial):
     o = torch.empty_like(Output_partial[:, :, 0, :]).fill_(0)
     lse_logsum = torch.empty_like(glse[:, :, 0]).fill_(0)  # [batch, heads]
@@ -188,7 +248,7 @@ def reduce_ref(Q, K, V, glse, Output_partial):
 
 
 if __name__ == "__main__":
-    BATCH, H, KV_H, Q_CTX, KV_CTX, D_HEAD = 1, 128, 1, 128, 128 * 1024, 128
+    BATCH, H, KV_H, Q_CTX, KV_CTX, D_HEAD = 1, 128, 1, 128, 8 * 1024, 128
     flops_per_matmul = 2.0 * BATCH * H * Q_CTX * KV_CTX * D_HEAD
     total_flops = 2 * flops_per_matmul
     BLOCK_N = 64  # if D_HEAD <= 128 else 32
@@ -198,4 +258,16 @@ if __name__ == "__main__":
     mod, params = tilelang.lower(program)
     mod = tilelang.Profiler(mod, params, [5], tilelang.TensorSupplyType.Normal)
     mod.assert_allclose(ref_program, rtol=0.01, atol=0.01)
-    print("All checks passed!")
+
+    # mod = tilelang.Profiler(mod, params, [3, 4], tilelang.TensorSupplyType.Normal)
+    # mod.assert_allclose(flash_split_ref, rtol=0.01, atol=0.01)
+
+    # ins = mod._get_inputs()
+    # outs = mod.func(*ins)
+    # ref_outs = reduce_ref(*ins)
+
+    # print("outs:", outs)
+    # print("ref_outs:", ref_outs)
+
+    # mod.assert_allclose(reduce_ref, rtol=0.01, atol=0.01)
+    # print("All checks passed!")
