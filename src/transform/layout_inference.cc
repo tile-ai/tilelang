88777c5995af4ed198963c286071566667b3b1c5
LeiWang1999 <leiwang1999@outlook.com>
2025-03-10 06:29:17 +0000

    [Refactor] Update Layout Inference Copyright and Simplify Return Logic
    - Replace Apache License header with Microsoft Corporation copyright notice
    - Simplify LayoutInference function by directly returning substituted function
    - Remove unnecessary variable assignment in return statement

--- src/transform/layout_inference.cc
+++ src/transform/layout_inference.cc
@@ -1,21 +1,5 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
 
 /*!
  * \file layout_inference.cc
@@ -431,8 +415,7 @@ tvm::transform::Pass LayoutInference() {
     collector(f->body);
     bool has_thread_binding = collector.thread_binding_.size() > 0;
     bool skip_thread_partition = !has_thread_binding;
-    auto _f = LayoutInferencer::Substitute(std::move(f), skip_thread_partition);
-    return _f;
+    return LayoutInferencer::Substitute(std::move(f), skip_thread_partition);
   };
   return CreatePrimFuncPass(pass_func, 0, "tl.LayoutInference", {});
 }
