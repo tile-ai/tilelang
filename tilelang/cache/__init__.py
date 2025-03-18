b84a4bc0a44fbf2c3d9e551210cefdcb0b1f65cd
xinyuxiao <xinyuxiao2024@gmail.com>
2025-03-18 08:27:50 +0000

    [Refactor] Update cached function signature to use PrimFunc and Optional types for improved type safety

--- tilelang/cache/__init__.py
+++ tilelang/cache/__init__.py
@@ -4,30 +4,32 @@
 
 from typing import Callable, List, Union
 from tvm.target import Target
+from tvm.tir import PrimFunc
 from tilelang.jit import JITKernel
 from .kernel_cache import KernelCache
-from typing import Callable, List, Literal, Union
+from typing import Callable, List, Literal, Union, Optional
 
 # Create singleton instance of KernelCache
 _kernel_cache_instance = KernelCache()
 
 
 def cached(
-    func: Callable,
+    func: PrimFunc=None,
     out_idx: List[int] = None,
     *args,
     target: Union[str, Target] = "auto",
     target_host: Union[str, Target] = None,
-    execution_backend: Literal["dlpack", "ctypes", "cython"] = "cython",
-    verbose: bool = False,
-    pass_configs: dict = None,
+    execution_backend: Optional[Literal["dlpack", "ctypes", "cython"]] = "cython",
+    verbose: Optional[bool] = False,
+    pass_configs: Optional[dict] = None,
 ) -> JITKernel:
     """
     Caches and reuses compiled kerne(ls (using KernelCache class).
     """
+    print(type(func))
     return _kernel_cache_instance.cached(
         func,
-        out_idx=out_idx,
+        out_idx,
         *args,
         target=target,
         target_host=target_host,
