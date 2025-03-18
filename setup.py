24172ab1368ecf8ee4610f1f817a536908222f79
LeiWang1999 <leiwang1999@outlook.com>
2025-03-18 13:27:36 +0000

    remove llvm build

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (c) Microsoft Corporation.
+# Copyright (c) Tile-AI Organization.
 # Licensed under the MIT License.
 
 import io
@@ -27,6 +27,8 @@ PYPI_BUILD = os.environ.get("PYPI_BUILD", "False").lower() == "true"
 PACKAGE_NAME = "tilelang"
 ROOT_DIR = os.path.dirname(__file__)
 
+# Add LLVM control environment variable
+USE_LLVM = os.environ.get("USE_LLVM", "False").lower() == "true"
 
 def load_module_from_path(module_name, path):
     spec = importlib.util.spec_from_file_location(module_name, path)
@@ -212,7 +214,7 @@ def build_csrc(llvm_config_path):
     # Run CMake and make
     try:
         subprocess.check_call(["cmake", ".."])
-        num_jobs = max(1, int(multiprocessing.cpu_count() * 0.9))
+        num_jobs = max(1, int(multiprocessing.cpu_count() * 0.75))
         subprocess.check_call(["make", f"-j{num_jobs}"])
     except subprocess.CalledProcessError as error:
         raise RuntimeError("Failed to build TileLang C Source") from error
@@ -423,6 +425,7 @@ class TileLangDevelopCommand(develop):
     """
 
     def run(self):
+        print("Running TileLangDevelopCommand")
         # 1. Build the C/C++ extension modules
         self.run_command("build_ext")
 
@@ -499,6 +502,16 @@ class CMakeBuild(build_ext):
         # Build each extension (of type CMakeExtension) using our custom method.
         for ext in self.extensions:
             self.build_cmake(ext)
+        
+        # To make it works with editable install, 
+        # we need to copy the lib*.so files to the tilelang/lib directory
+        import glob
+        files = glob.glob("*.so")
+        target_lib_dir = os.path.join(PACKAGE_NAME, "lib")
+        for file in files:
+            if not os.path.exists(target_lib_dir):
+                os.makedirs(target_lib_dir)
+            shutil.copy(file, target_lib_dir)
 
     def build_cmake(self, ext):
         """
@@ -506,9 +519,11 @@ class CMakeBuild(build_ext):
 
         :param ext: The extension (an instance of CMakeExtension).
         """
-        # Setup LLVM for TVM and retrieve the path to llvm-config.
-        # We assume the function returns (_, llvm_config_path).
-        _, llvm_config_path = setup_llvm_for_tvm()
+        # Only setup LLVM if it's enabled
+        llvm_config_path = "OFF"
+        if USE_LLVM:
+            # Setup LLVM for TVM and retrieve the path to llvm-config
+            _, llvm_config_path = setup_llvm_for_tvm()
 
         # Determine the directory where the final .so or .pyd library should go.
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
@@ -530,8 +545,7 @@ class CMakeBuild(build_ext):
         dst_config_cmake = os.path.join(build_temp, "config.cmake")
         shutil.copy(src_config_cmake, dst_config_cmake)
 
-        # Append some configuration variables to 'config.cmake'.
-        # Here, we set USE_LLVM and USE_CUDA, for example.
+        # Append some configuration variables to 'config.cmake'
         with open(dst_config_cmake, "a") as config_file:
             config_file.write(f"set(USE_LLVM {llvm_config_path})\n")
             config_file.write(f"set(USE_CUDA {CUDA_HOME})\n")
