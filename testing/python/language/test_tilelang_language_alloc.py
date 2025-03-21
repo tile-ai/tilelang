d801b4b27642f74aca6e066d2f77fc165473d64b
LeiWang1999 <leiwang1999@outlook.com>
2025-03-21 16:39:53 +0000

    lint fix

--- testing/python/language/test_tilelang_language_alloc.py
+++ testing/python/language/test_tilelang_language_alloc.py
@@ -2,7 +2,7 @@
 # Licensed under the MIT License.
 
 import tilelang.testing
-from tilelang.utils.tensor import map_torch_type
+
 
 def alloc_variable(
     N,
@@ -19,7 +19,7 @@ def alloc_variable(
         with T.Kernel(T.ceildiv(N, block_N), threads=block_N) as bx:
             A_shared = T.alloc_shared([block_N], dtype)
             tmp = T.alloc_variable(dtype)
-            tmp = 1
+            tmp = 1  # noqa: F841
             T.copy(A[bx * block_N], A_shared)
             T.copy(A_shared, B[bx * block_N])
 
@@ -36,12 +36,13 @@ def run_alloc_variable(
     program = alloc_variable(N, block_N, dtype)
 
     kernel = tilelang.compile(program, out_idx=[1])
-    profiler = kernel.get_profiler()
     code = kernel.get_kernel_source()
     assert "tmp =" in code
 
+
 def test_alloc_variable():
     run_alloc_variable(1024, 128, "float16")
+
 
 def alloc_variable_add(
     N,
@@ -58,7 +59,7 @@ def alloc_variable_add(
         with T.Kernel(T.ceildiv(N, block_N), threads=block_N) as bx:
             A_shared = T.alloc_shared([block_N], dtype)
             tmp = T.alloc_variable(dtype)
-            tmp = 1
+            tmp = 1  # noqa: F841
             T.copy(A[bx * block_N], A_shared)
             for i in T.Parallel(block_N):
                 A_shared[i] = A_shared[i] + tmp
@@ -75,12 +76,13 @@ def run_alloc_variable_add(
     program = alloc_variable_add(N, block_N, dtype)
 
     kernel = tilelang.compile(program, out_idx=[1])
-    profiler = kernel.get_profiler()
     code = kernel.get_kernel_source()
     assert "tmp =" in code
 
+
 def test_alloc_variable_add():
     run_alloc_variable_add(1024, 128, "float16")
+
 
 if __name__ == "__main__":
     tilelang.testing.main()
