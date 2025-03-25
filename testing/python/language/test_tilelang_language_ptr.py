0a6fb1898fc2f07e4634f5d8243d6344c46f17fe
LeiWang1999 <leiwang1999@outlook.com>
2025-03-25 06:31:24 +0000

    lint fix

--- testing/python/language/test_tilelang_language_ptr.py
+++ testing/python/language/test_tilelang_language_ptr.py
@@ -10,14 +10,15 @@ from tilelang.utils import map_torch_type
 
 
 def matmul_test(M, N, K, block_M, block_N, block_K, dtype="float16", accum_dtype="float"):
+
     @T.prim_func
     def main(
-       a_ptr: T.ptr,
-       b_ptr: T.ptr,
-       c_ptr: T.ptr,
-       m: T.int32,
-       n: T.int32,
-       k: T.int32,
+        a_ptr: T.ptr,
+        b_ptr: T.ptr,
+        c_ptr: T.ptr,
+        m: T.int32,
+        n: T.int32,
+        k: T.int32,
     ):
         A = T.Tensor.from_ptr(a_ptr, (m, k), dtype)
         B = T.Tensor.from_ptr(b_ptr, (k, n), dtype)
@@ -27,7 +28,7 @@ def matmul_test(M, N, K, block_M, block_N, block_K, dtype="float16", accum_dtype
         with T.Kernel(T.ceildiv(N, block_N), T.ceildiv(M, block_M), threads=128) as (bx, by):
             A_shared = T.alloc_shared((block_M, block_K), dtype)
             B_shared = T.alloc_shared((block_N, block_K), dtype)
-            C_local  = T.alloc_fragment((block_M, block_N), accum_dtype)
+            C_local = T.alloc_fragment((block_M, block_N), accum_dtype)
 
             T.clear(C_local)
 
@@ -45,7 +46,6 @@ def matmul_test(M, N, K, block_M, block_N, block_K, dtype="float16", accum_dtype
 def run_matmul(M, N, K, block_M, block_N, block_K, dtype="float16", accum_dtype="float"):
     program = matmul_test(M, N, K, block_M, block_N, block_K, dtype, accum_dtype)
     jit_kernel = tl.compile(program, target="cuda", execution_backend="cython")
-    profiler = jit_kernel.get_profiler()
 
     def ref_program(a, b):
         return (a @ b.T).to(torch.float32)
@@ -56,14 +56,15 @@ def run_matmul(M, N, K, block_M, block_N, block_K, dtype="float16", accum_dtype=
     c = torch.zeros(M, N, device="cuda", dtype=map_torch_type(accum_dtype))
 
     jit_kernel(a.data_ptr(), b.data_ptr(), c.data_ptr(), M, N, K)
-    
+
     ref_c = (a @ b.T).to(map_torch_type(accum_dtype))
-    
+
     torch.testing.assert_close(c, ref_c, atol=1e-2, rtol=1e-2)
+
 
 def test_matmul():
     run_matmul(1024, 1024, 1024, 128, 128, 32)
 
 
 if __name__ == "__main__":
-    tilelang.testing.main() 
+    tilelang.testing.main()
