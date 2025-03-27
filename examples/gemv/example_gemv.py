afd56b38258c0b664795a12773b53999cf3d8360
hxwang <wang1570@e.ntu.edu.sg>
2025-03-27 15:18:17 +0800

    [example] use local register instead of smem

--- examples/gemv/example_gemv.py
+++ examples/gemv/example_gemv.py
@@ -1,6 +1,7 @@
 # Copyright (c) Tile-AI Corporation.
 # Licensed under the MIT License.
 import argparse
+import itertools
 import torch
 import tilelang as tl
 import tilelang.language as T
@@ -11,6 +12,8 @@ from tilelang.carver.template import GEMVTemplate
 from tilelang.carver.arch import CUDA
 from tilelang.carver.roller.rasterization import NoRasterization
 
+def ref_program(A, B):
+    return A @ B.T
 
 def naive_gemv(
     N: int,
@@ -69,16 +72,18 @@ def naive_splitk_gemv(
         with T.Kernel(T.ceildiv(N, BLOCK_N), threads=(BLOCK_N, BLOCK_K)) as bn:
             tn = T.get_thread_binding(0)
             tk = T.get_thread_binding(1)
-            A_shared = T.alloc_shared((BLOCK_K,), dtype)
-            B_shared = T.alloc_shared((BLOCK_N, BLOCK_K), dtype)
+            A_local = T.alloc_local((1,), dtype)
+            B_local = T.alloc_local((1,), dtype)
+            C_accum = T.alloc_local((1,), accum_dtype)
             C_shared = T.alloc_shared((BLOCK_N,), accum_dtype)
             if tk == 0:
                 C_shared[tn] = 0
+            T.clear(C_accum)
             for bk in T.serial(T.ceildiv(K, BLOCK_K)):
-                A_shared[tk] = A[bk * BLOCK_K + tk]
-                B_shared[tn, tk] = B[bn * BLOCK_N + tn, bk * BLOCK_K + tk]
-                T.atomic_add(C_shared[tn],
-                             A_shared[tk].astype(accum_dtype) * B_shared[tn, tk].astype(accum_dtype))  # AtomicAdd as defined in src/tl_templates/cuda/common.h
+                A_local[0] = A[bk * BLOCK_K + tk]
+                B_local[0] = B[bn * BLOCK_N + tn, bk * BLOCK_K + tk]
+                C_accum[0] += A_local[0].astype(accum_dtype) * B_local[0].astype(accum_dtype)
+            T.atomic_add(C_shared[tn], C_accum[0])
             C[bn * BLOCK_N + tn] = C_shared[tn]
 
     return main
@@ -96,6 +101,7 @@ def splitk_gemv(
     """
     Naive GEMV following GEMM tiling strategy in SIMD manner.
     """
+    TILE_K = T.ceildiv(BLOCK_K, reduce_threads)
 
     @T.prim_func
     def main(
@@ -106,24 +112,21 @@ def splitk_gemv(
         with T.Kernel(T.ceildiv(N, BLOCK_N), threads=(BLOCK_N, reduce_threads)) as bn:
             tn = T.get_thread_binding(0)
             tk = T.get_thread_binding(1)
-            TILE_K = T.ceildiv(BLOCK_K, reduce_threads)
-            A_shared = T.alloc_shared((BLOCK_K,), dtype)
-            B_shared = T.alloc_shared((BLOCK_N, BLOCK_K), dtype)
+            A_local = T.alloc_local((TILE_K,), dtype)
+            B_local = T.alloc_local((TILE_K,), dtype)
             C_shared = T.alloc_shared((BLOCK_N,), accum_dtype)
+            C_accum = T.alloc_local((1,), accum_dtype)
             if tk == 0:
                 C_shared[tn] = 0
+            T.clear(C_accum)
             for bk in T.serial(T.ceildiv(K, BLOCK_K)):
                 for k in T.serial(TILE_K):
-                    A_shared[tk * TILE_K + k] = A[bk * BLOCK_K + tk * TILE_K + k]
-                    B_shared[tn, tk * TILE_K + k] = B[bn * BLOCK_N + tn,
-                                                      bk * BLOCK_K + tk * TILE_K + k]
-                C_reg = T.alloc_local((1,), accum_dtype)
-                T.clear(C_reg)
+                    A_local[k] = A[bk * BLOCK_K + tk * TILE_K + k]
+                    B_local[k] = B[bn * BLOCK_N + tn,
+                                                    bk * BLOCK_K + tk * TILE_K + k]
                 for k in T.serial(TILE_K):
-                    C_reg[0] += A_shared[tk * TILE_K +
-                                         k].astype(accum_dtype) * B_shared[tn, tk * TILE_K +
-                                                                           k].astype(accum_dtype)
-                T.atomic_add(C_shared[tn], C_reg[0])
+                    C_accum[0] += A_local[k].astype(accum_dtype) * B_local[k].astype(accum_dtype)
+            T.atomic_add(C_shared[tn], C_accum[0])
             C[bn * BLOCK_N + tn] = C_shared[tn]
 
     return main
@@ -134,6 +137,7 @@ def splitk_gemv_vectorized(
     K: int,
     BLOCK_N: int,
     reduce_threads: int,
+    num_stages: int,
     dtype: str = "float16",
     accum_dtype: str = "float",
 ):
@@ -150,30 +154,28 @@ def splitk_gemv_vectorized(
         with T.Kernel(T.ceildiv(N, BLOCK_N), threads=(BLOCK_N, reduce_threads)) as bn:
             tn = T.get_thread_binding(0)
             tk = T.get_thread_binding(1)
-            TILE_K = T.ceildiv(BLOCK_K, reduce_threads)
-            A_shared = T.alloc_shared((BLOCK_K,), dtype)
-            B_shared = T.alloc_shared((BLOCK_N, BLOCK_K), dtype)
+            A_local = T.alloc_local((TILE_K,), dtype)
+            B_local = T.alloc_local((TILE_K,), dtype)
             C_shared = T.alloc_shared((BLOCK_N,), accum_dtype)
+            C_accum = T.alloc_local((1,), accum_dtype)
             if tk == 0:
                 C_shared[tn] = 0
-            for bk in T.serial(T.ceildiv(K, BLOCK_K)):
+            T.clear(C_accum)
+            for bk in T.Pipelined(T.ceildiv(K, BLOCK_K), num_stages=num_stages):
                 for k in T.vectorized(TILE_K):
-                    A_shared[tk * TILE_K + k] = A[bk * BLOCK_K + tk * TILE_K + k]
-                    B_shared[tn, tk * TILE_K + k] = B[bn * BLOCK_N + tn,
-                                                      bk * BLOCK_K + tk * TILE_K + k]
-                C_reg = T.alloc_local((1,), accum_dtype)
-                T.clear(C_reg)
+                    A_local[k] = A[bk * BLOCK_K + tk * TILE_K + k]
+                    B_local[k] = B[bn * BLOCK_N + tn,
+                                                    bk * BLOCK_K + tk * TILE_K + k]
                 for k in T.serial(TILE_K):
-                    C_reg[0] += A_shared[tk * TILE_K +
-                                         k].astype(accum_dtype) * B_shared[tn, tk * TILE_K +
-                                                                           k].astype(accum_dtype)
-                T.atomic_add(C_shared[tn], C_reg[0])
+                    C_accum[0] += A_local[k].astype(accum_dtype) * B_local[k].astype(accum_dtype)
+            T.atomic_add(C_shared[tn], C_accum[0])
             C[bn * BLOCK_N + tn] = C_shared[tn]
 
     return main
 
 
-def check_correctness_and_bench(kernel, N, K, bench_ref=False):
+
+def check_correctness_and_bench(kernel, N, K, bench_ref=True):
     kernel = tl.compile(kernel, out_idx=-1)
     a = torch.randn(K).cuda().half()
     b = torch.randn(N, K).cuda().half()
@@ -183,9 +185,9 @@ def check_correctness_and_bench(kernel, N, K, bench_ref=False):
     profiler.assert_allclose(lambda x, y: x @ y.T, atol=1e-2, rtol=1e-2)
     if bench_ref:
         latency = profiler.do_bench(lambda x, y: x @ y.T, warmup=500)
-        print(f"Torch Latency: {latency:.2f} ms")
+        print(f"Torch Latency: {latency} ms")
     latency = profiler.do_bench(kernel, warmup=500)
-    print(f"TileLang Latency: {latency:.2f} ms")
+    print(f"TileLang Latency: {latency} ms\n")
 
 
 if __name__ == "__main__":
@@ -194,8 +196,8 @@ if __name__ == "__main__":
     parser.add_argument("--k", type=int, default=1024, help="Matrix dimension K")
     args = parser.parse_args()
     N, K = args.n, args.k
-    check_correctness_and_bench(naive_gemv(N, K, 128, 128), N, K, bench_ref=True)
+    check_correctness_and_bench(naive_gemv(N, K, 128, 128), N, K)
     check_correctness_and_bench(naive_splitk_gemv(N, K, 32, 32), N, K)
     check_correctness_and_bench(splitk_gemv(N, K, 32, 32, 32), N, K)
-    check_correctness_and_bench(splitk_gemv_vectorized(N, K, 32, 16), N, K)
+    check_correctness_and_bench(splitk_gemv_vectorized(N, K, 2, 32, 2), N, K)
     print("Test passed!")
