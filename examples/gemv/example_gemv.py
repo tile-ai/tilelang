f297ce44ae8186d2587da6eed381421ee493cb39
hxwang <wang1570@e.ntu.edu.sg>
2025-03-28 13:41:24 +0800

    [example] use tmv_all_reduce

--- examples/gemv/example_gemv.py
+++ examples/gemv/example_gemv.py
@@ -159,6 +159,60 @@ def splitk_gemv_vectorized(
     return main
 
 
+def splitk_gemv_vectorized_tvm(
+    N: int,
+    K: int,
+    BLOCK_N: int,
+    reduce_threads: int,
+    dtype: str = "float16",
+    accum_dtype: str = "float",
+):
+    MAX_TRANSACTION_SIZE_IN_BITS = 128
+    TILE_K = MAX_TRANSACTION_SIZE_IN_BITS // DataType(dtype).bits
+    BLOCK_K = reduce_threads * TILE_K
+
+    @T.prim_func
+    def main(
+            A: T.Buffer((K,), dtype),
+            B: T.Buffer((N, K), dtype),
+            C: T.Buffer((N,), dtype),
+    ):
+        with T.Kernel(T.ceildiv(N, BLOCK_N), threads=(BLOCK_N, reduce_threads)) as bn:
+            tn = T.get_thread_binding(0)
+            tk = T.get_thread_binding(1)
+            A_local = T.alloc_local((TILE_K,), dtype)
+            B_local = T.alloc_local((TILE_K,), dtype)
+            C_accum = T.alloc_local((1,), accum_dtype)
+
+            T.clear(C_accum)
+            for bk in T.serial(T.ceildiv(K, BLOCK_K)):
+                for k in T.vectorized(TILE_K):
+                    A_local[k] = A[bk * BLOCK_K + tk * TILE_K + k]
+                    B_local[k] = B[bn * BLOCK_N + tn, bk * BLOCK_K + tk * TILE_K + k]
+                for k in T.serial(TILE_K):
+                    C_accum[0] += A_local[k].astype(accum_dtype) * B_local[k].astype(accum_dtype)
+            C_reduced = T.alloc_local((1,), accum_dtype)
+            with T.attr(
+                    T.comm_reducer(lambda x, y: x + y, [T.Cast(accum_dtype, 0)]),
+                    "reduce_scope",
+                    T.reinterpret(T.uint64(0), dtype="handle"),
+            ):
+                T.evaluate(
+                    T.tvm_thread_allreduce(
+                        T.uint32(1),
+                        C_accum[0],
+                        True,
+                        C_reduced[0],
+                        tk,
+                        dtype="handle",
+                    ))
+
+            C[bn * BLOCK_N + tn] = C_reduced[0]
+
+    return main
+
+
+
 def get_best_config(N, K):
 
     def get_configs():
@@ -211,10 +265,8 @@ def get_best_config(N, K):
                 tk = T.get_thread_binding(1)
                 A_local = T.alloc_local((TILE_K,), dtype)
                 B_local = T.alloc_local((TILE_K,), dtype)
-                C_shared = T.alloc_shared((BLOCK_N,), accum_dtype)
                 C_accum = T.alloc_local((1,), accum_dtype)
-                if tk == 0:
-                    C_shared[tn] = 0
+
                 T.clear(C_accum)
                 for bk in T.serial(T.ceildiv(K, BLOCK_K)):
                     for k in T.vectorized(TILE_K):
@@ -222,8 +274,23 @@ def get_best_config(N, K):
                         B_local[k] = B[bn * BLOCK_N + tn, bk * BLOCK_K + tk * TILE_K + k]
                     for k in T.serial(TILE_K):
                         C_accum[0] += A_local[k].astype(accum_dtype) * B_local[k].astype(accum_dtype)
-                T.atomic_add(C_shared[tn], C_accum[0])
-                C[bn * BLOCK_N + tn] = C_shared[tn]
+                C_reduced = T.alloc_local((1,), accum_dtype)
+                with T.attr(
+                        T.comm_reducer(lambda x, y: x + y, [T.Cast(accum_dtype, 0)]),
+                        "reduce_scope",
+                        T.reinterpret(T.uint64(0), dtype="handle"),
+                ):
+                    T.evaluate(
+                        T.tvm_thread_allreduce(
+                            T.uint32(1),
+                            C_accum[0],
+                            True,
+                            C_reduced[0],
+                            tk,
+                            dtype="handle",
+                        ))
+
+                C[bn * BLOCK_N + tn] = C_reduced[0]
 
         return main
 
@@ -251,10 +318,11 @@ if __name__ == "__main__":
     check_correctness_and_bench(naive_splitk_gemv(N, K, 32, 32), N, K)
     check_correctness_and_bench(splitk_gemv(N, K, 32, 32, 32), N, K)
     check_correctness_and_bench(splitk_gemv_vectorized(N, K, 2, 32), N, K)
+    check_correctness_and_bench(splitk_gemv_vectorized_tvm(N, K, 2, 32), N, K)
     print("Test passed!")
 
     best_latency, best_config, ref_latency = get_best_config(N, K)
-    kernel = splitk_gemv_vectorized(N, K, *best_config)
+    kernel = splitk_gemv_vectorized_tvm(N, K, *best_config)
     kernel = tl.compile(kernel, out_idx=-1)
     profiler = kernel.get_profiler()
     latency = profiler.do_bench(lambda x, y: x @ y.T, warmup=500)
