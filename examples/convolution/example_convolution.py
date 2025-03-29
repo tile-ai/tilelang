068bdc5ca2d6743a300338b0ed0891ff1c5f9bfe
yyttt6 <1652272478@qq.com>
2025-03-29 03:56:51 +0000

    add autotune to conv

--- examples/convolution/example_convolution.py
+++ examples/convolution/example_convolution.py
@@ -6,8 +6,6 @@ from tilelang.autotuner import *
 import tilelang.language as T
 import itertools
 import argparse
-from functools import partial
-
 
 def check_hopper():
     # if not torch.cuda.is_available():
@@ -31,12 +29,20 @@ def get_configs():
         'block_N': c[1],
         'block_K': c[2],
         'num_stages': c[3],
-        'threads': c[4]
+        'thread_num': c[4]
     } for c in _configs]
     return configs
 
+def ref_program(stride, padding, dilation):
+    def main(A, B):
+        A = A.permute(0, 3, 1, 2)  # N, H, W, C -> N, C, H, W
+        B = B.permute(3, 2, 0, 1)  # H, W, C, F -> F, C, H, W
+        C = torch.conv2d(A, B, stride=stride, padding=padding, dilation=dilation)
+        C = C.permute(0, 2, 3, 1)  # N, C, H, W -> N, H, W, C
+        return C
+    return main
 
-def convolution(N, C, H, W, F, K, S, D, P, tune=False):
+def get_best_config(N, C, H, W, F, K, S, D, P):
     KH, KW = K, K
     OH = (H + 2 * P - D * (K - 1) - 1) // S + 1
     OW = (W + 2 * P - D * (K - 1) - 1) // S + 1
@@ -45,8 +51,14 @@ def convolution(N, C, H, W, F, K, S, D, P, tune=False):
     accum_dtype = "float"
     is_hopper = check_hopper()
 
-    def kernel_func(block_M, block_N, block_K, num_stages, threads):
 
+    def kernel(
+        block_M=None,
+        block_N=None,
+        block_K=None,
+        num_stages=None,
+        thread_num=None,
+    ):
         @T.prim_func
         def main(
                 data: T.Tensor((N, H, W, C), dtype),
@@ -55,7 +67,7 @@ def convolution(N, C, H, W, F, K, S, D, P, tune=False):
         ):
             with T.Kernel(
                     T.ceildiv(F, block_N), T.ceildiv(N * OH * OW, block_M),
-                    threads=threads) as (bx, by):
+                    threads=thread_num) as (bx, by):
                 data_shared = T.alloc_shared((block_M, block_K), dtype)
                 kernel_shared = T.alloc_shared((block_K, block_N), dtype)
                 out_local = T.alloc_fragment((block_M, block_N), accum_dtype)
@@ -91,33 +103,84 @@ def convolution(N, C, H, W, F, K, S, D, P, tune=False):
                 T.copy(out_shared, out_flat[by * block_M, bx * block_N])
 
         return main
+ 
+    autotuner = AutoTuner.from_kernel(
+        kernel=kernel, configs=get_configs()).set_compile_args(
+            out_idx=[2],
+            supply_type=tilelang.TensorSupplyType.Integer,
+            ref_prog=ref_program(S, P, D),
+            skip_check=False,
+            target="auto",
+        )
+    return autotuner.run(warmup=10, rep=10)
 
-    if tune:
+def convolution(N, 
+                C, 
+                H,
+                W, 
+                F, 
+                K, 
+                S, 
+                D, 
+                P,
+                block_M, 
+                block_N, 
+                block_K, 
+                num_stages, 
+                threads,
+                dtype="float16",
+                accum_dtype="float"):
+    KH, KW = K, K
+    OH = (H + 2 * P - D * (K - 1) - 1) // S + 1
+    OW = (W + 2 * P - D * (K - 1) - 1) // S + 1
+    dtype = "float16"
+    accum_dtype = "float"
+    is_hopper = check_hopper()
+    
+    @T.prim_func
+    def main(
+            data: T.Tensor((N, H, W, C), dtype),
+            kernel: T.Tensor((KH, KW, C, F), dtype),
+            out: T.Tensor((N, OH, OW, F), dtype),
+    ):
+        with T.Kernel(
+                T.ceildiv(F, block_N), T.ceildiv(N * OH * OW, block_M),
+                threads=threads) as (bx, by):
+            data_shared = T.alloc_shared((block_M, block_K), dtype)
+            kernel_shared = T.alloc_shared((block_K, block_N), dtype)
+            out_local = T.alloc_fragment((block_M, block_N), accum_dtype)
+            out_shared = T.alloc_shared((block_M, block_N), dtype)
 
-        @autotune(
-            configs=get_configs(),
-            keys=["block_M", "block_N", "block_K", "num_stages", "threads"],
-            warmup=10,
-            rep=10)
-        @jit(out_idx=[2], supply_type=tilelang.TensorSupplyType.Integer, ref_prog=None)
-        def kernel(block_M=None, block_N=None, block_K=None, num_stages=None, threads=None):
-            return kernel_func(block_M, block_N, block_K, num_stages, threads)
+            kernel_flat = T.Tensor((KH * KW * C, F), dtype, kernel.data)
+            out_flat = T.Tensor((N * OH * OW, F), dtype, out.data)
 
-        return kernel()
-    else:
+            T.annotate_layout({
+                out_shared: tilelang.layout.make_swizzled_layout(out_shared),
+                data_shared: tilelang.layout.make_swizzled_layout(data_shared),
+                kernel_shared: tilelang.layout.make_swizzled_layout(kernel_shared),
+            })
 
-        def kernel(block_M, block_N, block_K, num_stages, threads):
-            return kernel_func(block_M, block_N, block_K, num_stages, threads)
+            T.clear(out_local)
+            for k_iter in T.Pipelined(T.ceildiv(KH * KW * C, block_K), num_stages=num_stages):
+                if is_hopper:
+                    T.c2d_im2col(data, data_shared, by, k_iter, KH, S, D, P)
+                else:
+                    for i, j in T.Parallel(block_M, block_K):
+                        k = k_iter * block_K + j
+                        m = by * block_M + i
+                        access_h = m % (OH * OW) // OW * S + k // (KW * C) * D - P
+                        access_w = m % OW * S + k // C % KW * D - P
+                        in_bound = ((access_h >= 0) and (access_w >= 0) and (access_h < H) and
+                                    (access_w < W))
+                        data_shared[i, j] = T.if_then_else(
+                            in_bound, data[m // (OH * OW), access_h, access_w, k % C], 0)
+                T.copy(kernel_flat[k_iter * block_K, bx * block_N], kernel_shared)
+                T.gemm(data_shared, kernel_shared, out_local)
 
-        return kernel
+            T.copy(out_local, out_shared)
+            T.copy(out_shared, out_flat[by * block_M, bx * block_N])
 
-
-def ref_program(A, B, stride, padding, dilation):
-    A = A.permute(0, 3, 1, 2)  # N, H, W, C -> N, C, H, W
-    B = B.permute(3, 2, 0, 1)  # H, W, C, F -> F, C, H, W
-    C = torch.conv2d(A, B, stride=stride, padding=padding, dilation=dilation)
-    C = C.permute(0, 2, 3, 1)  # N, C, H, W -> N, H, W, C
-    return C
+    return main
 
 
 if __name__ == "__main__":
@@ -131,31 +194,24 @@ if __name__ == "__main__":
     parser.add_argument('--s', type=int, default=1, help='s')
     parser.add_argument('--d', type=int, default=1, help='d')
     parser.add_argument('--p', type=int, default=1, help='p')
-    parser.add_argument('--tune', action='store_true', help='tune configs')
+    parser.add_argument(
+        "--use_autotune",
+        action="store_true",
+        default=False,
+        help="Whether to use autotune for matmul configs")
+    
     args = parser.parse_args()
     N, C, H, W, F, K, S, D, P = args.n, args.c, args.h, args.w, args.f, args.k, args.s, args.d, args.p
-    OH = (H + 2 * P - D * (K - 1) - 1) // S + 1
-    OW = (W + 2 * P - D * (K - 1) - 1) // S + 1
-    total_flops = 2 * N * C * OH * OW * F * K * K
-
-    if (not args.tune):
-        program = convolution(
-            N, C, H, W, F, K, S, D, P, tune=args.tune)(
-                block_M=256, block_N=128, block_K=64, num_stages=4, threads=256)
-        ref_program = partial(ref_program, stride=S, padding=P, dilation=D)
-        kernel = tilelang.compile(program, out_idx=[2])
-        profiler = kernel.get_profiler(tilelang.TensorSupplyType.Normal)
-        profiler.assert_allclose(ref_program, rtol=0.01, atol=0.01)
-        print("All checks pass.")
-        latency = profiler.do_bench(ref_program, warmup=500)
-        print("Ref: {:.2f} ms".format(latency))
-        print("Ref: {:.2f} TFlops".format(total_flops / latency * 1e-9))
-        latency = profiler.do_bench(warmup=500)
-        print("Tile-lang: {:.2f} ms".format(latency))
-        print("Tile-lang: {:.2f} TFlops".format(total_flops / latency * 1e-9))
+    a = torch.randn(N, H, W, C).cuda().half()
+    b = torch.randn(K, K, C, F).cuda().half()
+    use_autotune = args.use_autotune
+    if use_autotune:
+        result = get_best_config(N, C, H, W, F, K, S, D, P)
+        print(f"best latency {result.latency}")
+        kernel = result.kernel
     else:
-        best_latency, best_config, ref_latency = convolution(
-            N, C, H, W, F, K, S, D, P, tune=args.tune)
-        print(f"Best latency: {best_latency}")
-        print(f"Best TFlops: {total_flops / best_latency * 1e-9}")
-        print(f"Best config: {best_config}")
+        kernel = tilelang.compile(convolution(N, C, H, W, F, K, S, D, P, 64, 128, 32, 3, 256), out_idx=[2])
+        
+    out_c = kernel(a, b)
+    ref_c = ref_program(S, P, D)(a, b)
+    torch.testing.assert_close(out_c, ref_c, rtol=1e-2, atol=1e-2)
