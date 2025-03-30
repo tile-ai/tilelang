3e0e0051c6a73c24468156b0faddc48fe8c18e32
LeiWang1999 <leiwang1999@outlook.com>
2025-03-30 09:21:04 +0000

    lintfix

--- src/tl_templates/cuda/gemm_sm89.h
+++ src/tl_templates/cuda/gemm_sm89.h
@@ -20,105 +20,93 @@ using _X = Underscore;
 
 #if (defined(__CUDA_ARCH_LIST__) && (__CUDA_ARCH_LIST__ >= 890))
 
-struct SM89_16x8x32_F32F8F8F32_E4M3_TN
-{
-    using DRegisters = float[4];
-    using ARegisters = uint32_t[4];
-    using BRegisters = uint32_t[2];
-    using CRegisters = float[4];
+struct SM89_16x8x32_F32F8F8F32_E4M3_TN {
+  using DRegisters = float[4];
+  using ARegisters = uint32_t[4];
+  using BRegisters = uint32_t[2];
+  using CRegisters = float[4];
 
-    CUTE_HOST_DEVICE static void
-    fma(float    & d0, float      & d1, float      & d2, float      & d3,
-        uint32_t const& a0, uint32_t const& a1, uint32_t const& a2, uint32_t const& a3,
-        uint32_t const& b0, uint32_t const& b1,
-        float const& c0, float const& c1, float const& c2, float const& c3)
-    {
-      asm volatile(
-      "mma.sync.aligned.m16n8k32.row.col.f32.e4m3.e4m3.f32 "
-      "{%0,  %1,  %2,  %3},"
-      "{%4,  %5,  %6,  %7},"
-      "{%8,  %9},"
-      "{%10, %11, %12, %13};\n"
-      :  "=f"(d0), "=f"(d1), "=f"(d2), "=f"(d3)
-      :  "r"(a0),  "r"(a1),  "r"(a2),  "r"(a3),
-      "r"(b0),  "r"(b1),
-      "f"(c0),  "f"(c1),  "f"(c2),  "f"(c3));
-    }
+  CUTE_HOST_DEVICE static void fma(float &d0, float &d1, float &d2, float &d3,
+                                   uint32_t const &a0, uint32_t const &a1,
+                                   uint32_t const &a2, uint32_t const &a3,
+                                   uint32_t const &b0, uint32_t const &b1,
+                                   float const &c0, float const &c1,
+                                   float const &c2, float const &c3) {
+    asm volatile("mma.sync.aligned.m16n8k32.row.col.f32.e4m3.e4m3.f32 "
+                 "{%0,  %1,  %2,  %3},"
+                 "{%4,  %5,  %6,  %7},"
+                 "{%8,  %9},"
+                 "{%10, %11, %12, %13};\n"
+                 : "=f"(d0), "=f"(d1), "=f"(d2), "=f"(d3)
+                 : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(b0), "r"(b1),
+                   "f"(c0), "f"(c1), "f"(c2), "f"(c3));
+  }
 };
 
-struct SM89_16x8x32_F32F8F8F32_E5M2_TN
-{
-    using DRegisters = float[4];
-    using ARegisters = uint32_t[4];
-    using BRegisters = uint32_t[2];
-    using CRegisters = float[4];
+struct SM89_16x8x32_F32F8F8F32_E5M2_TN {
+  using DRegisters = float[4];
+  using ARegisters = uint32_t[4];
+  using BRegisters = uint32_t[2];
+  using CRegisters = float[4];
 
-    CUTE_HOST_DEVICE static void
-    fma(float    & d0, float      & d1, float      & d2, float      & d3,
-        uint32_t const& a0, uint32_t const& a1, uint32_t const& a2, uint32_t const& a3,
-        uint32_t const& b0, uint32_t const& b1,
-        float const& c0, float const& c1, float const& c2, float const& c3)
-    {
-      asm volatile(
-              "mma.sync.aligned.m16n8k32.row.col.f32.e5m2.e5m2.f32 "
-              "{%0,  %1,  %2,  %3},"
-              "{%4,  %5,  %6,  %7},"
-              "{%8,  %9},"
-              "{%10, %11, %12, %13};\n"
-              : "=f"(d0), "=f"(d1), "=f"(d2), "=f"(d3)
-              :  "r"(a0),  "r"(a1),  "r"(a2),  "r"(a3),
-      "r"(b0),  "r"(b1),
-      "f"(c0),  "f"(c1),  "f"(c2),  "f"(c3));
-    }
+  CUTE_HOST_DEVICE static void fma(float &d0, float &d1, float &d2, float &d3,
+                                   uint32_t const &a0, uint32_t const &a1,
+                                   uint32_t const &a2, uint32_t const &a3,
+                                   uint32_t const &b0, uint32_t const &b1,
+                                   float const &c0, float const &c1,
+                                   float const &c2, float const &c3) {
+    asm volatile("mma.sync.aligned.m16n8k32.row.col.f32.e5m2.e5m2.f32 "
+                 "{%0,  %1,  %2,  %3},"
+                 "{%4,  %5,  %6,  %7},"
+                 "{%8,  %9},"
+                 "{%10, %11, %12, %13};\n"
+                 : "=f"(d0), "=f"(d1), "=f"(d2), "=f"(d3)
+                 : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(b0), "r"(b1),
+                   "f"(c0), "f"(c1), "f"(c2), "f"(c3));
+  }
 };
 
 // (T32,V1) -> (M8,N8)
-using SM80_8x4      = Layout<Shape <Shape < _4,_8>,_1>,
-                             Stride<Stride< _8,_1>,_0>>;
+using SM80_8x4 = Layout<Shape<Shape<_4, _8>, _1>, Stride<Stride<_8, _1>, _0>>;
 // (T32,V2) -> (M8,N8)
-using SM80_8x8_Row  = Layout<Shape <Shape < _4,_8>,_2>,
-                             Stride<Stride<_16,_1>,_8>>;
+using SM80_8x8_Row =
+    Layout<Shape<Shape<_4, _8>, _2>, Stride<Stride<_16, _1>, _8>>;
 // (T32,V4) -> (M8,N16)
-using SM80_8x16_Row = Layout<Shape <Shape < _4,_8>,_4>,
-                             Stride<Stride<_32,_1>,_8>>;
+using SM80_8x16_Row =
+    Layout<Shape<Shape<_4, _8>, _4>, Stride<Stride<_32, _1>, _8>>;
 // (T32,V4) -> (M16,N8)
-using SM80_16x8_Row = Layout<Shape <Shape < _4,_8>,Shape < _2,_2>>,
-                             Stride<Stride<_32,_1>,Stride<_16,_8>>>;
+using SM80_16x8_Row = Layout<Shape<Shape<_4, _8>, Shape<_2, _2>>,
+                             Stride<Stride<_32, _1>, Stride<_16, _8>>>;
 
-template <>
-struct MMA_Traits<SM89_16x8x32_F32F8F8F32_E4M3_TN>
-{
-     using ValTypeD = float;
-     using ValTypeA = fp8_e4_t;
-     using ValTypeB = fp8_e4_t;
-     using ValTypeC = float;
+template <> struct MMA_Traits<SM89_16x8x32_F32F8F8F32_E4M3_TN> {
+  using ValTypeD = float;
+  using ValTypeA = fp8_e4_t;
+  using ValTypeB = fp8_e4_t;
+  using ValTypeC = float;
 
-     using Shape_MNK = Shape<_16,_8,_32>;
-     using ThrID   = Layout<_32>;
-     using ALayout = Layout<Shape <Shape < _4,_8>,Shape < _4,_2,  _2>>,
-     Stride<Stride<_64,_1>,Stride<_16,_8,_256>>>;
-     using BLayout = Layout<Shape <Shape < _4,_8>, Shape <_4,  _2>>,
-     Stride<Stride<_32,_1>, Stride<_8,_128>>>;
-     using CLayout = SM80_16x8_Row;
+  using Shape_MNK = Shape<_16, _8, _32>;
+  using ThrID = Layout<_32>;
+  using ALayout = Layout<Shape<Shape<_4, _8>, Shape<_4, _2, _2>>,
+                         Stride<Stride<_64, _1>, Stride<_16, _8, _256>>>;
+  using BLayout = Layout<Shape<Shape<_4, _8>, Shape<_4, _2>>,
+                         Stride<Stride<_32, _1>, Stride<_8, _128>>>;
+  using CLayout = SM80_16x8_Row;
 };
 
-template <>
-struct MMA_Traits<SM89_16x8x32_F32F8F8F32_E5M2_TN>
-{
-    using ValTypeD = float;
-    using ValTypeA = fp8_e5_t;
-    using ValTypeB = fp8_e5_t;
-    using ValTypeC = float;
+template <> struct MMA_Traits<SM89_16x8x32_F32F8F8F32_E5M2_TN> {
+  using ValTypeD = float;
+  using ValTypeA = fp8_e5_t;
+  using ValTypeB = fp8_e5_t;
+  using ValTypeC = float;
 
-    using Shape_MNK = Shape<_16,_8,_32>;
-    using ThrID   = Layout<_32>;
-    using ALayout = Layout<Shape <Shape < _4,_8>,Shape < _4,_2,  _2>>,
-    Stride<Stride<_64,_1>,Stride<_16,_8,_256>>>;
-    using BLayout = Layout<Shape <Shape < _4,_8>, Shape <_4,  _2>>,
-    Stride<Stride<_32,_1>, Stride<_8,_128>>>;
-    using CLayout = SM80_16x8_Row;
+  using Shape_MNK = Shape<_16, _8, _32>;
+  using ThrID = Layout<_32>;
+  using ALayout = Layout<Shape<Shape<_4, _8>, Shape<_4, _2, _2>>,
+                         Stride<Stride<_64, _1>, Stride<_16, _8, _256>>>;
+  using BLayout = Layout<Shape<Shape<_4, _8>, Shape<_4, _2>>,
+                         Stride<Stride<_32, _1>, Stride<_8, _128>>>;
+  using CLayout = SM80_16x8_Row;
 };
-
 
 template <int num_warp_m, int num_warp_n>
 struct DispatchInstruction<fp8_e4_t, fp8_e4_t, float, num_warp_m, num_warp_n> {
