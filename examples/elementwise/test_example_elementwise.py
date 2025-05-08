701ffeba343c7c43088828c0e211bb897f778539
Cunxiao2002 <cunxiaoni@gmail.com>
2025-05-08 04:07:51 -0900

    test

--- examples/elementwise/test_example_elementwise.py
+++ examples/elementwise/test_example_elementwise.py
@@ -1,13 +1,18 @@
 # Copyright (c) Tile-AI Corporation.
 # Licensed under the MIT License.
 import tilelang.testing
+import sys
 
 import example_elementwise_add
 
 
-@tilelang.testing.requires_cuda
 def test_example_elementwise_add():
-    example_elementwise_add.main()
+    original_argv = sys.argv.copy()
+    try:
+        sys.argv = [sys.argv[0]]
+        example_elementwise_add.main()
+    finally:
+        sys.argv = original_argv
 
 
 if __name__ == "__main__":
