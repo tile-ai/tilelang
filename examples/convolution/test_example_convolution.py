e37c8f866efcf0617796d112933581e0eee212ce
xwhzz <wh.xie@outlook.com>
2025-05-10 02:46:15 +0000

    add convolution example to CI

---
+++ examples/convolution/test_example_convolution.py
@@ -0,0 +1,16 @@
+# Copyright (c) Tile-AI Corporation.
+# Licensed under the MIT License.
+import tilelang.testing
+
+import example_convolution
+
+
+
+@tilelang.testing.requires_cuda
+@tilelang.testing.requires_cuda_compute_version_eq(9, 0)
+def test_example_convolution():
+    example_convolution.main()
+
+
+if __name__ == "__main__":
+    tilelang.testing.main()
