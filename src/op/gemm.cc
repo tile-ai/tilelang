31d7d98a06d1ee970c05cc83347db054d1d1af4d
LeiWang1999 <leiwang1999@outlook.com>
2025-05-12 15:56:46 +0000

    [Refactor] Enhance makeGemmFragmentA and makeGemmFragmentB for transposition support
    * Updated the `makeGemmFragmentA` and `makeGemmFragmentB` functions to include a `transposed` parameter, allowing for flexible layout generation based on matrix transposition.
    * Adjusted layout calculations for both transposed and non-transposed cases to ensure correct fragment generation.
    * Modified function signatures in `layout.h` and updated all relevant calls in `gemm.cc` to accommodate the new parameter.
    * Added a new `matmul_rs` function in the test suite to validate the behavior of the updated fragment generation with transposition support.
    *

--- src/op/gemm.cc
+++ src/op/gemm.cc
@@ -210,9 +210,8 @@ LayoutMap Gemm::InferLayout(const LayoutInferArgs &T, InferLevel level) {
                   makeGemmABLayout(mat_stride, mat_continuous, mat_continuous,
                                    A->dtype.bits(), trans_A ? 1 : 2));
     } else if (A.scope() == "local.fragment") {
-      ICHECK(trans_A == false);
-      auto fragment =
-          makeGemmFragmentA(M, N, K, M / warp_m, N / warp_n, A->dtype.bits());
+      auto fragment = makeGemmFragmentA(M, N, K, M / warp_m, N / warp_n,
+                                        A->dtype.bits(), trans_A);
       results.Set(A, fragment->BindThreadRange(thread_range));
     } else {
       ICHECK(0);
@@ -225,7 +224,8 @@ LayoutMap Gemm::InferLayout(const LayoutInferArgs &T, InferLevel level) {
                   makeGemmABLayout(mat_stride, mat_continuous, mat_continuous,
                                    B->dtype.bits(), trans_B ? 2 : 1));
     } else if (B.scope() == "local.fragment") {
-      auto fragment = makeGemmFragmentB(M, N, K, M / warp_m, N / warp_n, trans_B);
+      auto fragment =
+          makeGemmFragmentB(M, N, K, M / warp_m, N / warp_n, trans_B);
       results.Set(B, fragment->BindThreadRange(thread_range));
     } else {
       ICHECK(0);
@@ -296,7 +296,8 @@ LayoutMap Gemm::InferLayout(const LayoutInferArgs &T, InferLevel level) {
 
       results.Set(B, shared_layout);
     } else if (B.scope() == "local.fragment") {
-      auto fragment = makeGemmFragmentB(M, N, K, M / warp_m, N / warp_n, trans_B);
+      auto fragment =
+          makeGemmFragmentB(M, N, K, M / warp_m, N / warp_n, trans_B);
       results.Set(B, fragment->BindThreadRange(thread_range));
     } else {
       ICHECK(0);
