4145e1c1d481b47a3ae409cd3d39e21ca03f8160
Cunxiao Ni <85601223+Cunxiao2002@users.noreply.github.com>
2025-06-25 23:24:26 +0800

    [Example] Update examples to use @tilelang.jit (#597)
    * [Example] Update kernel compilation in examples to use @tilelang.jit
    
    - Refactored multiple examples to eliminate the use of `tilelang.compile` for kernel creation, directly invoking the functions instead.
    - Added `@tilelang.jit` decorators with appropriate output indices to enhance performance and maintainability.
    - Improved code clarity by simplifying the kernel invocation process across various examples, ensuring consistency in how kernels are defined and executed.
    
    * format
    
    * Update example_tilelang_sparse_gqa_decode_varlen_indice.py
    
    * Update example_dequant_gemm_fine_grained.py
    
    * Update example_gemm_autotune.py
    
    ---------
    
    Co-authored-by: Lei Wang <34334180+LeiWang1999@users.noreply.github.com>

--- examples/warp_specialize/example_warp_specialize_flashmla.py
+++ examples/warp_specialize/example_warp_specialize_flashmla.py
@@ -9,6 +9,7 @@ import argparse
 tilelang.disable_cache()
 
 
+@tilelang.jit(out_idx=[6])
 def flashattn(batch, heads, kv_head_num, seqlen_kv, dim, pe_dim, block_N, block_H, num_split):
     scale = (1.0 / (dim + pe_dim))**0.5 * 1.44269504  # log2(e)
     dtype = "float16"
