b18ecb3fb896dd325d5ddfd78494210d525b1602
LeiWang1999 <leiwang1999@outlook.com>
2025-07-02 13:44:08 +0000

    lint fix

--- examples/elementwise/example_elementwise_add.py
+++ examples/elementwise/example_elementwise_add.py
@@ -78,8 +78,7 @@ def main():
     else:
         # Default config
         config = {"block_M": 32, "block_N": 32, "threads": 128}
-        kernel = tilelang.compile(
-            elementwise_add(M, N, **config, in_dtype="float32", out_dtype="float32"), out_idx=-1)
+        kernel = elementwise_add(M, N, **config, in_dtype="float32", out_dtype="float32")
 
     out = kernel(a, b)
     torch.testing.assert_close(out, ref_program(a, b), rtol=1e-2, atol=1e-2)
