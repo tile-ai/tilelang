7337624732a363ea8a69c948ad834247ad275140
LeiWang1999 <leiwang1999@outlook.com>
2025-07-09 14:59:53 +0800

    lint fix

--- testing/python/language/test_tilelang_language_composable_index.py
+++ testing/python/language/test_tilelang_language_composable_index.py
@@ -6,6 +6,7 @@ import tilelang.testing
 import tilelang.language as T
 import torch
 
+
 # add decorator @tilelang.jit if you want to return a torch function
 # @tilelang.jit
 def tilelang_composable_copy(M, N, block_M, block_N, dtype="float16"):
