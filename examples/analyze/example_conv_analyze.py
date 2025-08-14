ad6c8ba52a7ae31aab4550ba0da87cdca8510f8b
Zihua Wu <13583761+lucifer1004@users.noreply.github.com>
2025-08-14 18:08:00 +0800

    chore: fix ruff

--- examples/analyze/example_conv_analyze.py
+++ examples/analyze/example_conv_analyze.py
@@ -96,10 +96,7 @@ def kernel(N,
 
 def main():
     my_func = kernel(N, C, H, W, F, K, S, D, P, 64, 128, 32, 3, 256)
-    if torch.version.hip is not None:
-        cuda_device = CDNA("hip")
-    else:
-        cuda_device = CUDA("cuda")
+    cuda_device = CDNA("cuda") if torch.version.hip is None else CUDA("hip")
     result = Analyzer.analysis(my_func, cuda_device)
     print(result)
     print(f"Analyzed FLOPs: {result.total_flops}")
