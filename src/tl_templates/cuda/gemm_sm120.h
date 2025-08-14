9b57a1d7b4022bb594ea8a1231fe9fcf77d67a4c
Yichen Yan <oraluben@outlook.com>
2025-08-14 18:09:25 +0800

    unify mma gemm

--- src/tl_templates/cuda/gemm_sm120.h
+++ src/tl_templates/cuda/gemm_sm120.h
@@ -1,9 +1,3 @@
 #pragma once
 
-#include "cuda_fp8.h"
-#include <cute/arch/mma_sm120.hpp>
-
-#define JUST_POC_FOR_SM120
-
-// SM120 supports same instruction set as SM80 for WMMA
-#include "gemm_sm80.h"
+#include "gemm_mma.h"
