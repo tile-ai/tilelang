1599ff16efbaf9100ded9fce68cb088518b2c667
LeiWang1999 <leiwang1999@outlook.com>
2025-08-15 14:06:06 +0800

    Refactor target attribute helper functions for improved clarity

--- src/target/utils.cc
+++ src/target/utils.cc
@@ -60,13 +60,6 @@ bool TargetIsSM120(Target target) {
   return arch >= 120 && arch < 130;
 }
 
-bool TargetIsSM120(Target target) {
-  if (!TargetIsCuda(target))
-    return false;
-  int arch = GetArchInt(target);
-  return arch >= 120 && arch < 130;
-}
-
 bool TargetIsCDNA(Target target) {
   if (!TargetIsRocm(target))
     return false;
