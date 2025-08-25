4c0f978da0081670e2bbf2cac2ac5b8d5ce42e2c
LeiWang1999 <leiwang1999@outlook.com>
2025-08-25 15:17:11 +0800

    Refactor `finalize_reducer` and `reduce` functions to remove redundant target checks. Simplified conditionals by retaining only the `TargetIsHopper` check, enhancing code clarity and maintainability.

--- src/op/finalize_reducer.cc
+++ src/op/finalize_reducer.cc
@@ -51,7 +51,7 @@ Stmt FinalizeReducer::Lower(const LowerArgs &T,
   int reducing_threads = extent;
   std::stringstream ss;
   auto thread_offset = T.thread_bounds->min;
-  if (TargetIsHopper(T.target) || TargetIsSm100(T.target)) {
+  if (TargetIsHopper(T.target)) {
     auto all_threads = T.thread_bounds->extent;
     ss << "tl::AllReduce<" << op_str << ", " << reducing_threads << ", " << 1
        << ", " << thread_offset << ", " << all_threads << ">::run_hopper";
