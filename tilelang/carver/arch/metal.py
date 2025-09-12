1ddccfe6810e46fb30ed68041780e14e67512312
Yichen Yan <wenji.yyc@alibaba-inc.com>
2025-09-12 16:48:53 +0800

    fmt

--- tilelang/carver/arch/metal.py
+++ tilelang/carver/arch/metal.py
@@ -1,4 +1,3 @@
-
 from tvm.target import Target
 from .arch_base import TileDevice
 
@@ -8,6 +7,7 @@ def is_metal_arch(arch: TileDevice) -> bool:
 
 
 class METAL(TileDevice):
+
     def __init__(self, target: Target | str):
         if isinstance(target, str):
             target = Target(target)
