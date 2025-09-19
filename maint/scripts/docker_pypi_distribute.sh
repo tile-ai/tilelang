16d5ca38ad4c13ce2b3b8ecb04946fc6a1e1b5a7
LeiWang1999 <leiwang1999@outlook.com>
2025-09-19 18:37:42 +0800

    [Build] Update CMake and Python environment settings
    - Added static linking flags for GCC and libstdc++ in CMakeLists.txt to enhance library linking.
    - Removed the cmake version requirement from pyproject.toml to allow for broader compatibility.
    - Updated the tox command in the Docker distribution script to include Python 3.8 for testing environments.

--- maint/scripts/docker_pypi_distribute.sh
+++ maint/scripts/docker_pypi_distribute.sh
@@ -4,6 +4,6 @@ docker build . -f "$(dirname "${BASH_SOURCE[0]}")/pypi.Dockerfile" --tag ${IMAGE
 
 install_pip="python3.9 -m pip install --upgrade pip && python3.9 -m pip install -r requirements-build.txt"
 
-tox_command="python3.9 -m tox -e py39-pypi,py310-pypi,py311-pypi,py312-pypi,audit_2_27"
+tox_command="python3.9 -m tox -e py38-pypi,py39-pypi,py310-pypi,py311-pypi,py312-pypi,audit_2_27"
 
 docker run --rm -v $(pwd):/tilelang ${IMAGE} /bin/bash -c "$install_pip && $tox_command"
