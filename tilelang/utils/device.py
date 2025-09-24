694a437778ea01b2dedd2a6a4f36286985bd21fa
Yichen Yan <wenji.yyc@alibaba-inc.com>
2025-09-24 09:59:22 +0800

    fmt

--- tilelang/utils/device.py
+++ tilelang/utils/device.py
@@ -4,11 +4,12 @@ import torch
 IS_CUDA = torch.cuda.is_available()
 IS_MPS = torch.mps.is_available()
 
+
 def get_current_device():
     device = None
     if IS_CUDA:
         device = torch.cuda.current_device()
     elif IS_MPS:
-        device = 'mps:0'
+        device = "mps:0"
 
     return device
