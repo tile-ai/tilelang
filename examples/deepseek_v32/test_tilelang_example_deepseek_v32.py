3858d81fd991bf4d09a0a046819d4e1f34e78b8a
LeiWang1999 <leiwang1999@outlook.com>
2025-10-10 00:25:32 +0800

    adjust test case

--- examples/deepseek_v32/test_tilelang_example_deepseek_v32.py
+++ examples/deepseek_v32/test_tilelang_example_deepseek_v32.py
@@ -36,7 +36,7 @@ def test_example_sparse_mla_fwd_pipelined():
 @tilelang.testing.requires_cuda_compute_version_ge(9, 0)
 def test_example_sparse_mla_bwd():
     test_sparse_mla_bwd(
-        S=1024, SKV=2048, H=128, HKV=1, DQKV=576, DV=512, topk=256, check_correctness=False)
+        S=1024, SKV=2048, H=64, HKV=1, DQKV=576, DV=512, topk=256, check_correctness=False)
 
 
 if __name__ == "__main__":
