47d7537a02b0b413bf2dbc9983c3e4a1f82be144
Yichen Yan <wenji.yyc@alibaba-inc.com>
2025-10-11 13:04:16 +0800

    update docs

--- cmake/FindTVM.cmake
+++
@@ -1,8 +0,0 @@
-# todo: support prebuilt tvm
-
-# Outputs:
-# TVM_FOUND
-# TVM_SOURCE
-
-set(TVM_FOUND FALSE)
-set(TVM_SOURCE ${CMAKE_SOURCE_DIR}/3rdparty/tvm)
--- version_provider.py
+++ version_provider.py
@@ -11,20 +11,24 @@ ROOT = Path(__file__).parent
 base_version = (ROOT / 'VERSION').read_text().strip()
 
 
-def _read_cmake_bool(i: str | None):
-    return i and i.lower() not in ('0', 'false', 'off', 'no', 'n', '')
+def _read_cmake_bool(i: str | None, default =False):
+    if i is None:
+        return default
+    return i.lower() not in ('0', 'false', 'off', 'no', 'n', '')
 
 
 def get_git_commit_id() -> Optional[str]:
     """Get the current git commit hash by running git in the current file's directory."""
-    try:
-        return subprocess.check_output(['git', 'rev-parse', 'HEAD'],
-                                       cwd=os.path.dirname(os.path.abspath(__file__)),
-                                       stderr=subprocess.DEVNULL,
-                                       encoding='utf-8').strip()
-    # FileNotFoundError is raised when git is not installed
-    except (subprocess.SubprocessError, FileNotFoundError):
-        return None
+
+    r = subprocess.run(['git', 'rev-parse', 'HEAD'],
+                                    cwd=ROOT,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE,
+                                    encoding='utf-8').strip()
+    if r.returncode == 0:
+        return r.stdout
+    else:
+        return 'unknown'
 
 
 def dynamic_metadata(
@@ -33,35 +37,36 @@ def dynamic_metadata(
 ) -> str:
     assert field == 'version'
 
-    exts = []
-    backend = None
-    if _read_cmake_bool(os.environ.get('NO_TOOLCHAIN_VERSION')):
-        pass
-    elif platform.system() == 'Darwin':
-        backend = 'metal'
-    elif _read_cmake_bool(os.environ.get('USE_ROCM', '')):
-        backend = 'rocm'
-    elif 'USE_CUDA' in os.environ and not _read_cmake_bool(os.environ.get('USE_CUDA')):
-        backend = 'cpu'
-    else:  # cuda
-        # Read nvcc version from env.
-        # This is not exactly how it should be,
-        # but works for now if building in a nvidia/cuda image.
-        if cuda_version := os.environ.get('CUDA_VERSION'):
-            major, minor, *_ = cuda_version.split('.')
-            backend = f'cu{major}{minor}'
-        else:
-            backend = 'cuda'
-    if backend:
-        exts.append(backend)
-
-    if git_hash := get_git_commit_id():
-        exts.append(f'git{git_hash[:8]}')
-
     version = base_version
 
-    if exts:
-        version += '+' + '.'.join(exts)
+    if not _read_cmake_bool(os.environ.get('NO_VERSION_LABEL')):
+        exts = []
+        backend = None
+        if _read_cmake_bool(os.environ.get('NO_TOOLCHAIN_VERSION')):
+            pass
+        elif platform.system() == 'Darwin':
+            backend = 'metal'
+        elif _read_cmake_bool(os.environ.get('USE_ROCM', '')):
+            backend = 'rocm'
+        elif 'USE_CUDA' in os.environ and not _read_cmake_bool(os.environ.get('USE_CUDA')):
+            backend = 'cpu'
+        else:  # cuda
+            # Read nvcc version from env.
+            # This is not exactly how it should be,
+            # but works for now if building in a nvidia/cuda image.
+            if cuda_version := os.environ.get('CUDA_VERSION'):
+                major, minor, *_ = cuda_version.split('.')
+                backend = f'cu{major}{minor}'
+            else:
+                backend = 'cuda'
+        if backend:
+            exts.append(backend)
+
+        if git_hash := get_git_commit_id():
+            exts.append(f'git{git_hash[:8]}')
+
+        if exts:
+            version += '+' + '.'.join(exts)
 
     return version
 
