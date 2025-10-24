fdf46694616f7e6110ea3be2b94570a601ee3013
LeiWang1999 <leiwang1999@outlook.com>
2025-10-24 14:16:15 +0800

    atomic_fix

--- src/transform/atomicadd_vectorize.cc
+++ src/transform/atomicadd_vectorize.cc
@@ -231,11 +231,6 @@ private:
       // Ref: src/tl_templates/cuda/atomic.h::AtomicAdd
       const IntImm memory_order =
           node->args.size() >= 3 ? Downcast<IntImm>(node->args[2]) : IntImm(0);
-
-      Call address_of_dst =
-          Call(DataType::Handle(), builtin::address_of(), {dst_node});
-      Call address_of_value =
-          Call(DataType::Handle(), builtin::address_of(), {value_node});
       Array<PrimExpr> new_args;
       if (vector_size_ == 4) {
         new_args.push_back(StringImm("AtomicAddx4"));
@@ -244,8 +239,8 @@ private:
       } else {
         new_args.push_back(StringImm("AtomicAdd"));
       }
-      new_args.push_back(address_of_dst);
-      new_args.push_back(address_of_value);
+      new_args.push_back(dst_node);
+      new_args.push_back(value_node);
       new_args.push_back(memory_order);
 
       Call new_call =
