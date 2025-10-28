4d0bc855f3e2a262714e4cb83899ac7dbe380227
Freebase6912 <amid-gauze-racing@duck.com>
2025-10-28 16:58:02 +0800

    fix lint error

--- testing/python/language/test_tilelang_language_dtype.py
+++ testing/python/language/test_tilelang_language_dtype.py
@@ -4,33 +4,35 @@ import torch
 import tilelang.testing
 import tvm
 
+
 def test_argument():
+
     @T.prim_func
     def test_argument(
-            t_1: T.bool,
-            t_2: T.short,
-            t_3: T.int,
-            t_4: T.long,
-            t_5: T.half,
-            t_6: T.float,
-            t_7: T.long,
-            t_8: T.int8,
-            t_9: T.int16,
-            t_10: T.int32,
-            t_11: T.int64,
-            t_12: T.uint8,
-            t_13: T.uint16,
-            t_14: T.uint32,
-            t_15: T.uint64,
-            t_16: T.float8_e4m3fn,
-            t_17: T.float8_e4m3fnuz,
-            t_18: T.float8_e5m2,
-            t_19: T.float8_e5m2fnuz,
-            t_20: T.float8_e8m0fnu,
-            t_21: T.float16,
-            t_22: T.bfloat16,
-            t_23: T.float32,
-            t_24: T.float64,
+        t_1: T.bool,
+        t_2: T.short,
+        t_3: T.int,
+        t_4: T.long,
+        t_5: T.half,
+        t_6: T.float,
+        t_7: T.long,
+        t_8: T.int8,
+        t_9: T.int16,
+        t_10: T.int32,
+        t_11: T.int64,
+        t_12: T.uint8,
+        t_13: T.uint16,
+        t_14: T.uint32,
+        t_15: T.uint64,
+        t_16: T.float8_e4m3fn,
+        t_17: T.float8_e4m3fnuz,
+        t_18: T.float8_e5m2,
+        t_19: T.float8_e5m2fnuz,
+        t_20: T.float8_e8m0fnu,
+        t_21: T.float16,
+        t_22: T.bfloat16,
+        t_23: T.float32,
+        t_24: T.float64,
     ):
         pass
 
@@ -44,14 +46,15 @@ def test_expr():
         try:
             dtype(1.0)
             dtype()
-        except TypeError as e:
+        except TypeError:
             pass
-        except Exception as e:
+        except Exception:
             errors.append(name)
     assert not errors
 
 
 def test_var_decl_sugar():
+
     @T.prim_func
     def test_var_decl_sugar():
         with T.Kernel(128, 128) as (bx, by):
@@ -107,35 +110,38 @@ def test_var_decl_sugar():
     s = test_var_decl_sugar.script()
     for i in range(1, 25):
         assert f'var_{i}_1' in s
-        assert f'tl.local_var_init' in s
+        assert 'tl.local_var_init' in s
+
 
 def test_dtype_str_repr():
+
     @T.prim_func
     def test_str_repr():
-        buf_1 = T.alloc_buffer((1,), dtype=T.bool, scope='shared')
-        buf_2 = T.alloc_buffer((1,), dtype=T.short, scope='shared')
-        buf_3 = T.alloc_buffer((1,), dtype=T.int, scope='shared')
-        buf_4 = T.alloc_buffer((1,), dtype=T.long, scope='shared')
-        buf_5 = T.alloc_buffer((1,), dtype=T.half, scope='shared')
-        buf_6 = T.alloc_buffer((1,), dtype=T.float, scope='shared')
-        buf_7 = T.alloc_buffer((1,), dtype=T.long, scope='shared')
-        buf_8 = T.alloc_buffer((1,), dtype=T.int8, scope='shared')
-        buf_9 = T.alloc_buffer((1,), dtype=T.int16, scope='shared')
-        buf_10 = T.alloc_buffer((1,), dtype=T.int32, scope='shared')
-        buf_11 = T.alloc_buffer((1,), dtype=T.int64, scope='shared')
-        buf_12 = T.alloc_buffer((1,), dtype=T.uint8, scope='shared')
-        buf_13 = T.alloc_buffer((1,), dtype=T.uint16, scope='shared')
-        buf_14 = T.alloc_buffer((1,), dtype=T.uint32, scope='shared')
-        buf_15 = T.alloc_buffer((1,), dtype=T.uint64, scope='shared')
-        buf_16 = T.alloc_buffer((1,), dtype=T.float8_e4m3fn, scope='shared')
-        buf_17 = T.alloc_buffer((1,), dtype=T.float8_e4m3fnuz, scope='shared')
-        buf_18 = T.alloc_buffer((1,), dtype=T.float8_e5m2, scope='shared')
-        buf_19 = T.alloc_buffer((1,), dtype=T.float8_e5m2fnuz, scope='shared')
-        buf_20 = T.alloc_buffer((1,), dtype=T.float8_e8m0fnu, scope='shared')
-        buf_21 = T.alloc_buffer((1,), dtype=T.float16, scope='shared')
-        buf_22 = T.alloc_buffer((1,), dtype=T.bfloat16, scope='shared')
-        buf_23 = T.alloc_buffer((1,), dtype=T.float32, scope='shared')
-        buf_24 = T.alloc_buffer((1,), dtype=T.float64, scope='shared')
+        buf_1 = T.alloc_buffer((1,), dtype=T.bool, scope='shared')  # noqa F841
+        buf_2 = T.alloc_buffer((1,), dtype=T.short, scope='shared')  # noqa F841
+        buf_3 = T.alloc_buffer((1,), dtype=T.int, scope='shared')  # noqa F841
+        buf_4 = T.alloc_buffer((1,), dtype=T.long, scope='shared')  # noqa F841
+        buf_5 = T.alloc_buffer((1,), dtype=T.half, scope='shared')  # noqa F841
+        buf_6 = T.alloc_buffer((1,), dtype=T.float, scope='shared')  # noqa F841
+        buf_7 = T.alloc_buffer((1,), dtype=T.long, scope='shared')  # noqa F841
+        buf_8 = T.alloc_buffer((1,), dtype=T.int8, scope='shared')  # noqa F841
+        buf_9 = T.alloc_buffer((1,), dtype=T.int16, scope='shared')  # noqa F841
+        buf_10 = T.alloc_buffer((1,), dtype=T.int32, scope='shared')  # noqa F841
+        buf_11 = T.alloc_buffer((1,), dtype=T.int64, scope='shared')  # noqa F841
+        buf_12 = T.alloc_buffer((1,), dtype=T.uint8, scope='shared')  # noqa F841
+        buf_13 = T.alloc_buffer((1,), dtype=T.uint16, scope='shared')  # noqa F841
+        buf_14 = T.alloc_buffer((1,), dtype=T.uint32, scope='shared')  # noqa F841
+        buf_15 = T.alloc_buffer((1,), dtype=T.uint64, scope='shared')  # noqa F841
+        buf_16 = T.alloc_buffer((1,), dtype=T.float8_e4m3fn, scope='shared')  # noqa F841
+        buf_17 = T.alloc_buffer((1,), dtype=T.float8_e4m3fnuz, scope='shared')  # noqa F841
+        buf_18 = T.alloc_buffer((1,), dtype=T.float8_e5m2, scope='shared')  # noqa F841
+        buf_19 = T.alloc_buffer((1,), dtype=T.float8_e5m2fnuz, scope='shared')  # noqa F841
+        buf_20 = T.alloc_buffer((1,), dtype=T.float8_e8m0fnu, scope='shared')  # noqa F841
+        buf_21 = T.alloc_buffer((1,), dtype=T.float16, scope='shared')  # noqa F841
+        buf_22 = T.alloc_buffer((1,), dtype=T.bfloat16, scope='shared')  # noqa F841
+        buf_23 = T.alloc_buffer((1,), dtype=T.float32, scope='shared')  # noqa F841
+        buf_24 = T.alloc_buffer((1,), dtype=T.float64, scope='shared')  # noqa F841
+
 
 def test_torch_eq():
     dtypes = [
@@ -195,6 +201,7 @@ def test_torch_eq():
 
 
 def test_var_assign():
+
     @tilelang.jit(out_idx=-1)
     @T.prim_func
     def test_var_assign(A: T.Tensor((2,), T.int32)):
@@ -205,6 +212,7 @@ def test_var_assign():
             d: T.int32 = a
             A[0] = b
             A[1] = d
+
     res = test_var_assign()()
     assert res[0] == 1
     assert res[1] == 2
