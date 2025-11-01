5c4d8f118d499d3fec4b3dd7801c79ad5356ac04
Yuqi Dong <134183314+yyttt6@users.noreply.github.com>
2025-11-01 13:38:16 +0800

    Update parameters in test_example_cast

--- examples/cast/test_example_cast.py
+++ examples/cast/test_example_cast.py
@@ -4,7 +4,7 @@ import example_per_token_cast_to_fp8
 
 
 def test_example_group_per_split_token_cast_to_fp8():
-    example_group_per_split_token_cast_to_fp8.main(M=4096, N=2048, BG=2, blk_m=8)
+    example_group_per_split_token_cast_to_fp8.main(M=8192, N=2048, BG=1, blk_m=4)
 
 
 def test_example_per_token_cast_to_fp8():
