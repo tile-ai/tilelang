271f965047454ad17b87f945ea081c38c20f6c05
Yichen Yan <wenji.yyc@alibaba-inc.com>
2025-11-02 11:56:19 +0800

    fmt

--- version_provider.py
+++ version_provider.py
@@ -14,7 +14,7 @@ base_version = (ROOT / 'VERSION').read_text().strip()
 # so pip will complain when we install `tilelang-0.1.6.post2+gitxxxx.tar.gz`.
 # To workaround that, when building sdist,
 # we do not add version label and use a file to store the git hash instead.
-git_pin = ROOT/'_git_commit.txt'
+git_pin = ROOT / '_git_commit.txt'
 
 
 def _read_cmake_bool(i: str | None, default=False):
@@ -86,7 +86,6 @@ def dynamic_metadata(
 
         if exts:
             version += '+' + '.'.join(exts)
-    
 
     return version
 
