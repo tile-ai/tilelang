ca8e74da9948e4e5bce5bfc3c6334be0a6a840c5
Freebase6912 <amid-gauze-racing@duck.com>
2025-11-10 11:39:15 +0800

    Fix Buffer re-import typo in tilelang.langugage

--- tilelang/language/__init__.py
+++ tilelang/language/__init__.py
@@ -82,7 +82,7 @@ from .customize import (
 )
 from .logical import any_of, all_of  # noqa: F401
 from .builtin import *  # noqa: F401
-
+from .proxy import Buffer # noqa: F401
 from .utils import index_to_coordinates  # noqa: F401
 
 from .symbolics import dynamic, symbolic  # noqa: F401
