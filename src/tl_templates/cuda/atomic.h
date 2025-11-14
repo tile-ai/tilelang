1cc34b7a30212cda786c583c198e5a84c7ff4aad
Zihua Wu <13583761+lucifer1004@users.noreply.github.com>
2025-11-14 19:06:11 +0800

    [fix] always use cuda::std::__atomic_ref_impl

--- src/tl_templates/cuda/atomic.h
+++ src/tl_templates/cuda/atomic.h
@@ -5,15 +5,13 @@
 #endif
 
 #if (defined(__CUDACC_VER_MAJOR__) && __CUDACC_VER_MAJOR__ >= 13)
-  #include <cccl/cuda/std/atomic>
-  // CUDA 13+ uses internal __atomic_ref_impl to specify thread_scope
-  template <typename T, cuda::std::thread_scope Scope>
-  using atomic_ref = cuda::std::__atomic_ref_impl<T, Scope>;
+#include <cccl/cuda/std/atomic>
 #else
-  #include <cuda/std/atomic>
-  template <typename T, typename Scope>
-  using atomic_ref = cuda::atomic_ref<T, Scope>;
+#include <cuda/std/atomic>
 #endif
+
+template <typename T, cuda::std::thread_scope Scope>
+using atomic_ref = cuda::std::__atomic_ref_impl<T, Scope>;
 
 #include <cuda_fp16.h>
 #include <cutlass/numeric_types.h>
