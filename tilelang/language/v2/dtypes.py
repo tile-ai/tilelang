ece1dc3a62ad08f431b147b2b24e2582b1510cd2
Freebase6912 <227995639+kurisu6912@users.noreply.github.com>
2025-11-14 14:01:35 +0800

    fix typo

--- tilelang/language/v2/dtypes.py
+++ tilelang/language/v2/dtypes.py
@@ -18,7 +18,7 @@ _PYTHON_DTYPE_TO_STR = {
 
 _NUMPY_DTYPE_TO_STR = {
     np.bool_: 'bool',
-    np.short: 'short',
+    np.short: 'int16',
     np.int_: 'int64',
     np.longlong: 'int64',
     np.half: 'float16',
