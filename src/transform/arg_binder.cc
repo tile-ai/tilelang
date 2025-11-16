8f59fa1c6349ca55f64e150f9b4b6e12d5ec2583
Lei Wang <leiwang1999@outlook.com>
2025-11-17 02:34:58 +0800

    lint fix

--- src/transform/arg_binder.cc
+++ src/transform/arg_binder.cc
@@ -197,7 +197,7 @@ void ArgBinder::BindDLTensor(const Buffer &buffer, const PrimExpr &device_type,
   init_nest_.emplace_back(
       LetStmt(is_null_var,
               Call(DataType::Bool(), builtin::isnullptr(), {handle}), nop));
-  const PrimExpr& is_null = is_null_var;
+  const PrimExpr &is_null = is_null_var;
 
   // dimension checks
   PrimExpr v_ndim = TVMArrayGet(tvm_ndim_type, handle, builtin::kArrNDim);
