82e425b6b519867caeecd692b28e976b71fd97ba
Freebase6912 <227995639+kurisu6912@users.noreply.github.com>
2025-11-19 13:55:21 +0800

    remove duplicated, because tilelang doesn't dependent deprecated

--- tilelang/language/v2/utils.py
+++ tilelang/language/v2/utils.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 import ast
 import inspect
 from typing import Any, Callable, Literal
-from deprecated import deprecated
 from tilelang import env
 from hashlib import sha256
 import linecache
@@ -52,28 +51,6 @@ def get_func_nonlocals(func):
                 if "empty" not in str(err):
                     raise
     return nonlocal_vars
-
-
-@deprecated("inspect_function_capture is dangerous and will be removed in future versions.")
-def inspect_function_capture(func: Callable) -> dict[str, Any]:
-    """Capture function non-locals and global variables.
-
-    Parameters
-    ----------
-    func : Callable
-        The function to inspect.
-
-    Returns
-    -------
-    res : Dict[str, Any]
-        The function variables map with non-local or global variables.
-    """
-    # dangerous: func.__globals__[..] are strong referenced and may cause memory leak
-    captured = {
-        **func.__globals__,  # type: ignore
-        **get_func_nonlocals(func),
-    }
-    return captured
 
 
 def get_ast(func: Callable):
