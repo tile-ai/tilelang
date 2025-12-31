5a899deee2f606f22ef00f8d4b61d20bc7ebdc39
Yichen Yan <oraluben@outlook.com>
2025-12-31 18:45:23 +0800

    fmt

--- src/target/codegen_metal.cc
+++ src/target/codegen_metal.cc
@@ -36,7 +36,7 @@
 namespace tvm {
 namespace codegen {
 
-void CodeGenMetal::InitFuncState(const PrimFunc& f) {
+void CodeGenMetal::InitFuncState(const PrimFunc &f) {
   CodeGenC::InitFuncState(f);
   // analyze the data;
   for (Var arg : f->params) {
@@ -54,7 +54,7 @@ CodeGenMetal::CodeGenMetal(Target target) : target_(target) {
               << "};\n\n";
 }
 
-void CodeGenMetal::AddFunction(const GlobalVar& gvar, const PrimFunc& func) {
+void CodeGenMetal::AddFunction(const GlobalVar &gvar, const PrimFunc &func) {
   // NOTE: There is no inter-function calls among Metal kernels.
   // For now we keep the metal codegen without inter-function call
   // process.
@@ -80,18 +80,22 @@ void CodeGenMetal::AddFunction(const GlobalVar& gvar, const PrimFunc& func) {
       << "CodeGenC: Expect PrimFunc to have the global_symbol attribute";
 
   // Function header.
-  this->stream << "kernel void " << static_cast<std::string>(global_symbol.value()) << "(";
+  this->stream << "kernel void "
+               << static_cast<std::string>(global_symbol.value()) << "(";
 
   // Buffer arguments
   size_t num_buffer = 0;
-  size_t limit = target_->GetAttr<Integer>("max_function_args").value().IntValue();
+  size_t limit =
+      target_->GetAttr<Integer>("max_function_args").value().IntValue();
   if (func->params.size() > limit) {
-    LOG(WARNING) << "Probably you won't be able to execute your kernel due to high number of "
+    LOG(WARNING) << "Probably you won't be able to execute your kernel due to "
+                    "high number of "
                     "buffers in the kernel";
   }
   for (size_t i = 0; i < func->params.size(); ++i, ++num_buffer) {
     Var v = func->params[i];
-    if (!v.dtype().is_handle()) break;
+    if (!v.dtype().is_handle())
+      break;
     this->stream << "  ";
     std::string vid = AllocVarID(v.get());
     auto it = alloc_storage_scope_.find(v.get());
@@ -102,8 +106,8 @@ void CodeGenMetal::AddFunction(const GlobalVar& gvar, const PrimFunc& func) {
     // Register handle data type
     // TODO(tvm-team): consider simply keep type info in the
     // type annotation(via a normalizing rewriting).
-    if (auto* ptr = v->type_annotation.as<PointerTypeNode>()) {
-      if (auto* prim = ptr->element_type.as<PrimTypeNode>()) {
+    if (auto *ptr = v->type_annotation.as<PointerTypeNode>()) {
+      if (auto *prim = ptr->element_type.as<PrimTypeNode>()) {
         RegisterHandleType(v.get(), prim->dtype);
       }
     }
@@ -113,9 +117,10 @@ void CodeGenMetal::AddFunction(const GlobalVar& gvar, const PrimFunc& func) {
   size_t nargs = func->params.size() - num_buffer;
   std::string varg = name_supply_->FreshName("arg");
   if (nargs != 0) {
-    std::string arg_buf_type = static_cast<std::string>(global_symbol.value()) + "_args_t";
-    this->stream << "  constant " << arg_buf_type << "& " << varg << " [[ buffer(" << num_buffer
-                 << ") ]],\n";
+    std::string arg_buf_type =
+        static_cast<std::string>(global_symbol.value()) + "_args_t";
+    this->stream << "  constant " << arg_buf_type << "& " << varg
+                 << " [[ buffer(" << num_buffer << ") ]],\n";
     // declare the struct
     decl_stream << "struct " << arg_buf_type << " {\n";
     for (size_t i = num_buffer; i < func->params.size(); ++i) {
@@ -148,8 +153,9 @@ void CodeGenMetal::AddFunction(const GlobalVar& gvar, const PrimFunc& func) {
   ICHECK_EQ(name_supply_->FreshName("blockIdx"), "blockIdx");
   int work_dim = 0;
   auto launch_params =
-      func->GetAttr<ffi::Array<ffi::String>>(tir::attr::kKernelLaunchParams).value();
-  for (const auto& tag : launch_params) {
+      func->GetAttr<ffi::Array<ffi::String>>(tir::attr::kKernelLaunchParams)
+          .value();
+  for (const auto &tag : launch_params) {
     if (tag != runtime::launch_param::kUseDynamicSharedMemoryTag) {
       runtime::ThreadScope scope = runtime::ThreadScope::Create(tag);
       work_dim = std::max(work_dim, scope.dim_index + 1);
@@ -176,7 +182,7 @@ void CodeGenMetal::AddFunction(const GlobalVar& gvar, const PrimFunc& func) {
   this->stream << "}\n\n";
 }
 
-void CodeGenMetal::BindThreadIndex(const IterVar& iv) {
+void CodeGenMetal::BindThreadIndex(const IterVar &iv) {
   ICHECK(!var_idmap_.count(iv->var.get()));
   // if we only have threadIdx.x
   // metal will directly print as threadIdx
@@ -188,7 +194,7 @@ void CodeGenMetal::BindThreadIndex(const IterVar& iv) {
       CastFromTo(vname, DataType::UInt(thread_index_bits_), iv->var.dtype());
 }
 
-void CodeGenMetal::PrintType(DataType t, std::ostream& os) {  // NOLINT(*)
+void CodeGenMetal::PrintType(DataType t, std::ostream &os) { // NOLINT(*)
   int lanes = t.lanes();
   if (t.is_handle()) {
     ICHECK_EQ(lanes, 1) << "do not yet support vector types";
@@ -206,29 +212,30 @@ void CodeGenMetal::PrintType(DataType t, std::ostream& os) {  // NOLINT(*)
   }
   bool fail = false;
   if (t.is_float()) {
-    // Need to care about sizes and alignment of half3/float3 because tir representation might not
-    // be aware of Metal half3/float3 details and can treat them as just three elements,
-    // while sizes and alignmnents of half3/float3 are one element more (half3-8 bytes/
-    // float13 - 16bytes).
-    // Example of problematic pattern: filling of threadgroup packed array using float3 elements
-    // by threads concurrently can lead to datarace and wrong data in threadgroup shared array.
-    // packed_(half3/float3) are exactly datatypes dealing with 3 elements and per-element
-    // alignment
+    // Need to care about sizes and alignment of half3/float3 because tir
+    // representation might not be aware of Metal half3/float3 details and can
+    // treat them as just three elements, while sizes and alignmnents of
+    // half3/float3 are one element more (half3-8 bytes/ float13 - 16bytes).
+    // Example of problematic pattern: filling of threadgroup packed array using
+    // float3 elements by threads concurrently can lead to datarace and wrong
+    // data in threadgroup shared array. packed_(half3/float3) are exactly
+    // datatypes dealing with 3 elements and per-element alignment
     if (lanes == 3) {
       os << "packed_";
     }
     switch (t.bits()) {
-      case 16:
-        os << "half";
-        break;
-      case 32:
-        os << "float";
-        break;
-      default:
-        fail = true;
-        break;
+    case 16:
+      os << "half";
+      break;
+    case 32:
+      os << "float";
+      break;
+    default:
+      fail = true;
+      break;
     }
-    if (!fail && lanes == 1) return;
+    if (!fail && lanes == 1)
+      return;
     if (!fail && (lanes >= 2 && lanes <= 4)) {
       os << lanes;
       return;
@@ -238,26 +245,27 @@ void CodeGenMetal::PrintType(DataType t, std::ostream& os) {  // NOLINT(*)
       os << 'u';
     }
     switch (t.bits()) {
-      case 8:
-        os << "char";
-        break;
-      case 16:
-        os << "short";
-        break;
-      case 32:
-        os << "int";
-        break;
-      case 64:
-        os << "long";
-        break;
-      case 1:
-        os << "bool";
-        break;
-      default:
-        fail = true;
-        break;
+    case 8:
+      os << "char";
+      break;
+    case 16:
+      os << "short";
+      break;
+    case 32:
+      os << "int";
+      break;
+    case 64:
+      os << "long";
+      break;
+    case 1:
+      os << "bool";
+      break;
+    default:
+      fail = true;
+      break;
     }
-    if (!fail && lanes == 1) return;
+    if (!fail && lanes == 1)
+      return;
     if (!fail && (lanes >= 2 && lanes <= 4)) {
       os << lanes;
       return;
@@ -269,8 +277,8 @@ void CodeGenMetal::PrintType(DataType t, std::ostream& os) {  // NOLINT(*)
   LOG(FATAL) << "Cannot convert type " << t << " to Metal type";
 }
 
-void CodeGenMetal::PrintStorageSync(const CallNode* op) {
-  const std::string& sync = op->args[0].as<StringImmNode>()->value;
+void CodeGenMetal::PrintStorageSync(const CallNode *op) {
+  const std::string &sync = op->args[0].as<StringImmNode>()->value;
   if (sync == "warp") {
     this->PrintIndent();
     this->stream << "simdgroup_barrier(mem_flags::mem_threadgroup);\n";
@@ -282,19 +290,20 @@ void CodeGenMetal::PrintStorageSync(const CallNode* op) {
   }
 }
 
-void CodeGenMetal::PrintVecElemLoad(const std::string& vec, DataType t, int i,
-                                    std::ostream& os) {  // NOLINT(*)
+void CodeGenMetal::PrintVecElemLoad(const std::string &vec, DataType t, int i,
+                                    std::ostream &os) { // NOLINT(*)
   os << vec << "[" << i << "]";
 }
 
-void CodeGenMetal::PrintVecElemStore(const std::string& vec, DataType t, int i,
-                                     const std::string& value) {
+void CodeGenMetal::PrintVecElemStore(const std::string &vec, DataType t, int i,
+                                     const std::string &value) {
   this->PrintIndent();
   stream << vec << "[" << i << "]"
          << " = " << value << ";\n";
 }
 
-void CodeGenMetal::PrintStorageScope(const std::string& scope, std::ostream& os) {  // NOLINT(*)
+void CodeGenMetal::PrintStorageScope(const std::string &scope,
+                                     std::ostream &os) { // NOLINT(*)
   if (scope == "global") {
     os << "device ";
   } else if (scope == "shared") {
@@ -306,28 +315,32 @@ void CodeGenMetal::PrintStorageScope(const std::string& scope, std::ostream& os)
   }
 }
 
-void CodeGenMetal::VisitStmt_(const AllocateNode* op) {
+void CodeGenMetal::VisitStmt_(const AllocateNode *op) {
   ICHECK(!is_zero(op->condition));
   std::string vid = AllocVarID(op->buffer_var.get());
 
   this->PrintIndent();
   size_t constant_size = op->ConstantAllocationSize();
-  ICHECK_GT(constant_size, 0) << "Can only handle constant size stack allocation for now";
+  ICHECK_GT(constant_size, 0)
+      << "Can only handle constant size stack allocation for now";
 
   auto scope = GetPtrStorageScope(op->buffer_var);
   alloc_storage_scope_[op->buffer_var.get()] = scope;
   if (scope == "metal.simdgroup") {
-    ICHECK(op->dtype == DataType::Float(16) || op->dtype == DataType::Float(32) ||
+    ICHECK(op->dtype == DataType::Float(16) ||
+           op->dtype == DataType::Float(32) ||
            op->dtype == DataType::BFloat(16))
-        << "Only float16, float32, and bfloat16 are supported, but got " << op->dtype;
-    ICHECK(constant_size % 64 == 0)
-        << "Only 8x8 matrix is supported, but got " << constant_size << " bytes\n";
+        << "Only float16, float32, and bfloat16 are supported, but got "
+        << op->dtype;
+    ICHECK(constant_size % 64 == 0) << "Only 8x8 matrix is supported, but got "
+                                    << constant_size << " bytes\n";
 
     std::ostringstream dtype_os;
     PrintType(op->dtype, dtype_os);
     std::string dtype_str = dtype_os.str();
     simdgroup_dtype_[op->buffer_var.get()] = dtype_str;
-    stream << "simdgroup_" << dtype_str << "8x8 " << vid << '[' << constant_size / 64 << "];\n";
+    stream << "simdgroup_" << dtype_str << "8x8 " << vid << '['
+           << constant_size / 64 << "];\n";
   } else {
     PrintStorageScope(scope, stream);
     PrintType(op->dtype, stream);
@@ -338,34 +351,41 @@ void CodeGenMetal::VisitStmt_(const AllocateNode* op) {
   this->PrintStmt(op->body);
 }
 
-void CodeGenMetal::VisitExpr_(const SelectNode* op, std::ostream& os) {  // NOLINT(*)
-  os << "select(" << PrintExpr(op->false_value) << ", " << PrintExpr(op->true_value) << ", "
-     << PrintExpr(op->condition) << ")";
+void CodeGenMetal::VisitExpr_(const SelectNode *op,
+                              std::ostream &os) { // NOLINT(*)
+  os << "select(" << PrintExpr(op->false_value) << ", "
+     << PrintExpr(op->true_value) << ", " << PrintExpr(op->condition) << ")";
 }
 
-void CodeGenMetal::VisitExpr_(const BroadcastNode* op, std::ostream& os) {  // NOLINT(*)
+void CodeGenMetal::VisitExpr_(const BroadcastNode *op,
+                              std::ostream &os) { // NOLINT(*)
   std::string v = PrintExpr(op->value);
   int lanes = op->dtype.lanes();
   PrintType(op->dtype, os);
   os << "(";
   for (int i = 0; i < lanes; ++i) {
-    if (i != 0) os << ", ";
+    if (i != 0)
+      os << ", ";
     os << v;
   }
   os << ')';
 }
 
-void CodeGenMetal::VisitExpr_(const CallNode* op, std::ostream& os) {  // NOLINT(*)
+void CodeGenMetal::VisitExpr_(const CallNode *op,
+                              std::ostream &os) { // NOLINT(*)
   CHECK(!op->op.as<GlobalVarNode>())
       << "CodegenMetal does not support inter-function calls, "
-      << "but expression " << ffi::GetRef<Call>(op) << " calls PrimFunc " << op->op;
+      << "but expression " << ffi::GetRef<Call>(op) << " calls PrimFunc "
+      << op->op;
   auto f_check_simdgroup_shape = [](PrimExpr col, PrimExpr row) {
     ICHECK(col->IsInstance<IntImmNode>() && row->IsInstance<IntImmNode>())
-        << "Only constant shape is supported for simdgroup matrix, but got " << col << "x" << row;
+        << "Only constant shape is supported for simdgroup matrix, but got "
+        << col << "x" << row;
     int col_val = col.as<IntImmNode>()->value;
     int row_val = row.as<IntImmNode>()->value;
     ICHECK(col_val == 8 && row_val == 8)
-        << "Only 8x8 matrix is supported, but got " << col_val << "x" << row_val;
+        << "Only 8x8 matrix is supported, but got " << col_val << "x"
+        << row_val;
   };
   if (op->op.same_as(builtin::make_filled_simdgroup_matrix())) {
     ICHECK_EQ(op->args.size(), 5);
@@ -374,29 +394,30 @@ void CodeGenMetal::VisitExpr_(const CallNode* op, std::ostream& os) {  // NOLINT
     auto it = simdgroup_dtype_.find(var.get());
     ICHECK(it != simdgroup_dtype_.end())
         << "Cannot find variable allocation for simdgroup: " << var;
-    const std::string& dtype_str = it->second;
+    const std::string &dtype_str = it->second;
     f_check_simdgroup_shape(op->args[3], op->args[4]);
-    os << PrintExpr(var) << "[" << PrintExpr(op->args[1]) << "] = make_filled_simdgroup_matrix<"
-       << dtype_str << ", " << PrintExpr(op->args[3]) << ", " << PrintExpr(op->args[4]) << ">("
+    os << PrintExpr(var) << "[" << PrintExpr(op->args[1])
+       << "] = make_filled_simdgroup_matrix<" << dtype_str << ", "
+       << PrintExpr(op->args[3]) << ", " << PrintExpr(op->args[4]) << ">("
        << PrintExpr(op->args[2]) << ")";
   } else if (op->op.same_as(builtin::simdgroup_load())) {
     ICHECK_EQ(op->args.size(), 7);
     f_check_simdgroup_shape(op->args[4], op->args[5]);
-    os << "simdgroup_load(" << PrintExpr(op->args[0]) << "[" << PrintExpr(op->args[1]) << "], "
-       << PrintExpr(op->args[2]) << ", " << PrintExpr(op->args[3]) << ", 0, "
-       << PrintExpr(op->args[6]) << ")";
+    os << "simdgroup_load(" << PrintExpr(op->args[0]) << "["
+       << PrintExpr(op->args[1]) << "], " << PrintExpr(op->args[2]) << ", "
+       << PrintExpr(op->args[3]) << ", 0, " << PrintExpr(op->args[6]) << ")";
   } else if (op->op.same_as(builtin::simdgroup_store())) {
     ICHECK_EQ(op->args.size(), 7);
     f_check_simdgroup_shape(op->args[4], op->args[5]);
-    os << "simdgroup_store(" << PrintExpr(op->args[0]) << "[" << PrintExpr(op->args[1]) << "], "
-       << PrintExpr(op->args[2]) << ", " << PrintExpr(op->args[3]) << ", 0, "
-       << PrintExpr(op->args[6]) << ")";
+    os << "simdgroup_store(" << PrintExpr(op->args[0]) << "["
+       << PrintExpr(op->args[1]) << "], " << PrintExpr(op->args[2]) << ", "
+       << PrintExpr(op->args[3]) << ", 0, " << PrintExpr(op->args[6]) << ")";
   } else if (op->op.same_as(builtin::simdgroup_multiply_accumulate())) {
     ICHECK_EQ(op->args.size(), 8);
-    os << "simdgroup_multiply_accumulate("                                  //
-       << PrintExpr(op->args[0]) << "[" << PrintExpr(op->args[1]) << "], "  //
-       << PrintExpr(op->args[2]) << "[" << PrintExpr(op->args[3]) << "], "  //
-       << PrintExpr(op->args[4]) << "[" << PrintExpr(op->args[5]) << "], "  //
+    os << "simdgroup_multiply_accumulate("                                 //
+       << PrintExpr(op->args[0]) << "[" << PrintExpr(op->args[1]) << "], " //
+       << PrintExpr(op->args[2]) << "[" << PrintExpr(op->args[3]) << "], " //
+       << PrintExpr(op->args[4]) << "[" << PrintExpr(op->args[5]) << "], " //
        << PrintExpr(op->args[6]) << "[" << PrintExpr(op->args[7]) << "])";
   } else if (op->op.same_as(builtin::reinterpret())) {
     // generate as_type<TYPE>(ARG)
@@ -410,7 +431,8 @@ void CodeGenMetal::VisitExpr_(const CallNode* op, std::ostream& os) {  // NOLINT
   }
 }
 
-void CodeGenMetal::VisitExpr_(const FloatImmNode* op, std::ostream& os) {  // NOLINT(*)
+void CodeGenMetal::VisitExpr_(const FloatImmNode *op,
+                              std::ostream &os) { // NOLINT(*)
   std::ostringstream temp;
   if (std::isinf(op->value)) {
     if (op->value < 0) {
@@ -430,5 +452,5 @@ void CodeGenMetal::VisitExpr_(const FloatImmNode* op, std::ostream& os) {  // NO
   os << temp.str();
 }
 
-}  // namespace codegen
-}  // namespace tvm
+} // namespace codegen
+} // namespace tvm
