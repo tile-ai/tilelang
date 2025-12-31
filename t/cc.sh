1d7c88e3ea79cc9924860fc7e429be98089de806
Your Name <wenji.yyc@alibaba-inc.com>
2025-12-31 21:27:29 +0800

    add current device/hsot code

---
+++ t/cc.sh
@@ -0,0 +1,6 @@
+
+/usr/bin/g++ -std=gnu++17 -shared -fPIC -undefined dynamic_lookup -o /dev/null -x objective-c++ lib0.c devc.c \
+    -I/Users/yyc/repo/tilelang/3rdparty/tvm/include -I/Users/yyc/repo/tilelang/.venv/lib/python3.12/site-packages/tvm_ffi/include \
+    -I/Users/yyc/repo/tilelang/3rdparty/tvm/3rdparty/tvm-ffi/3rdparty/dlpack/include \
+    -I/Users/yyc/repo/tilelang/3rdparty/tvm/3rdparty/dmlc-core/include -I/Users/yyc/repo/tilelang/.venv/lib/python3.12/site-packages/torch/include \
+    -I/Users/yyc/repo/tilelang/.venv/lib/python3.12/site-packages/torch/include/torch/csrc/api/include
