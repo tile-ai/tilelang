e5d845198d1840e711a5ec2aa0b4ff92380d380c
Zihua Wu <13583761+lucifer1004@users.noreply.github.com>
2026-01-03 22:21:29 -0800

    fix: add support for tanh/tanhf (fixes #1595)

--- tilelang/contrib/cutedsl/math.py
+++ tilelang/contrib/cutedsl/math.py
@@ -2,7 +2,7 @@ import cutlass.cute as cute
 from cutlass.cute.typing import Union, Numeric
 from cutlass.cute.tensor import TensorSSA
 from cutlass._mlir.dialects import arith
-from cutlass.cute.math import exp, exp2, log, log2, log10, tan, cos, sin, sqrt  # noqa: F401
+from cutlass.cute.math import exp, exp2, log, log2, log10, tan, cos, sin, sqrt, tanh  # noqa: F401
 
 
 def divf(x: Union[TensorSSA, Numeric], y: Union[TensorSSA, Numeric], fastmath: bool = False) -> Union[TensorSSA, Numeric]:
