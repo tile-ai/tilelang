38d0c755a211e94cf853291501347f94d2f0db75
Gongen-Ali <gongen.ge@alibaba-inc.com>
2026-01-05 17:30:29 +0800

    pre-cmmit fix

--- examples/gemm_fp8/example_tilelang_gemm_amd_fp8_preshuffle.py
+++ examples/gemm_fp8/example_tilelang_gemm_amd_fp8_preshuffle.py
@@ -7,9 +7,9 @@ import tilelang.language as T
 from tilelang.tileop.base import GemmWarpPolicy
 from tilelang.layout import make_swizzled_layout
 from tilelang.intrinsics.mfma_macro_generator import MatrixCorePreshuffleIntrinEmitter
-from tilelang.transform import simplify_prim_func
 
 tilelang.testing.set_random_seed(0)
+
 
 def get_configs():
     block_Ms = [32, 64, 128]
@@ -20,12 +20,14 @@ def get_configs():
     valid_configs = []
 
     for m, n, k, stages in itertools.product(block_Ms, block_Ns, block_Ks, num_stages):
-        valid_configs.append({
-            "block_M": m,
-            "block_N": n,
-            "block_K": k,
-            "num_stages": stages,
-        })
+        valid_configs.append(
+            {
+                "block_M": m,
+                "block_N": n,
+                "block_K": k,
+                "num_stages": stages,
+            }
+        )
     return valid_configs
 
 
@@ -37,7 +39,7 @@ def tl_matmul(
     M,
     N,
     K,
-    block_M, 
+    block_M,
     block_N,
     block_K,
     num_stages,
@@ -78,7 +80,7 @@ def tl_matmul(
     local_size_a = mfma_emitter.local_size_a
     local_size_b = mfma_emitter.local_size_b
     local_size_c = mfma_emitter.local_size_out
-    
+
     warp_rows = mfma_emitter.warp_rows
     warp_cols = mfma_emitter.warp_cols
 
@@ -90,33 +92,38 @@ def tl_matmul(
     A_shared_shape = (block_K, block_M) if a_transposed else (block_M, block_K)
 
     if b_preshuffle:
-        B_shape = (N // micro_size_y, K // pack_size_k, micro_size_y,
-                   pack_size_k) if b_transposed else (K // pack_size_k, N // micro_size_y,
-                                                      pack_size_k, micro_size_y)
-        B_shared_shape = (block_N // micro_size_y, block_K // pack_size_k, micro_size_y,
-                          pack_size_k) if b_transposed else (block_K // pack_size_k,
-                                                             block_N // micro_size_y, pack_size_k,
-                                                             micro_size_y)
+        B_shape = (
+            (N // micro_size_y, K // pack_size_k, micro_size_y, pack_size_k)
+            if b_transposed
+            else (K // pack_size_k, N // micro_size_y, pack_size_k, micro_size_y)
+        )
+        B_shared_shape = (
+            (block_N // micro_size_y, block_K // pack_size_k, micro_size_y, pack_size_k)
+            if b_transposed
+            else (block_K // pack_size_k, block_N // micro_size_y, pack_size_k, micro_size_y)
+        )
     else:
         B_shape = (N, K) if b_transposed else (K, N)
         B_shared_shape = (block_N, block_K) if b_transposed else (block_K, block_N)
-    
+
     @T.prim_func
     def main(
-            A: T.Tensor(A_shape, in_dtype),
-            B: T.Tensor(B_shape, in_dtype),
-            C: T.Tensor((M, N), out_dtype),
+        A: T.Tensor(A_shape, in_dtype),
+        B: T.Tensor(B_shape, in_dtype),
+        C: T.Tensor((M, N), out_dtype),
     ):
         with T.Kernel(T.ceildiv(N, block_N), T.ceildiv(M, block_M), threads=num_threads) as (bx, by):
             A_shared = T.alloc_shared(A_shared_shape, in_dtype, scope=shared_scope)
             A_local = T.alloc_local((warp_rows * local_size_a * k_pack), in_dtype)
             B_local = T.alloc_local((warp_cols * local_size_b * k_pack), in_dtype)
             C_local = T.alloc_fragment((block_M, block_N), accum_dtype)
-            
-            T.annotate_layout({
-                A_shared: make_swizzled_layout(A_shared),
-                C_local: mfma_emitter.make_mfma_store_layout(C_local),
-            })
+
+            T.annotate_layout(
+                {
+                    A_shared: make_swizzled_layout(A_shared),
+                    C_local: mfma_emitter.make_mfma_store_layout(C_local),
+                }
+            )
 
             num_ko = K // block_K
             num_ki = block_K // (k_pack * micro_size_k)
@@ -125,7 +132,6 @@ def tl_matmul(
             # T.use_swizzle(panel_size=10)
             T.clear(C_local)
             for ko in T.Pipelined(num_ko, num_stages=num_stages):
-
                 # Load A into shared memory
                 if a_transposed:
                     T.copy(A[ko * block_K, by * block_M], A_shared)
@@ -138,26 +144,21 @@ def tl_matmul(
                         A_shared,
                         ki,
                     )
-                    mfma_emitter.ldmatrix_b(
-                        B_local,
-                        B,
-                        ki + ko * num_ki,
-                        pid_m=by,
-                        pid_n=bx
-                    )
+                    mfma_emitter.ldmatrix_b(B_local, B, ki + ko * num_ki, pid_m=by, pid_n=bx)
 
                     # Perform Matrix Multiplication
                     mfma_emitter.mfma(A_local, B_local, C_local, ki)
 
             T.copy(C_local, C[by * block_M, bx * block_N])
+
     return main
 
 
 def shuffle_weight(
-        x: torch.Tensor,
-        layout=(16, 32),
-        k_pack=1,
-        is_transpose=False,
+    x: torch.Tensor,
+    layout=(16, 32),
+    k_pack=1,
+    is_transpose=False,
 ) -> torch.Tensor:
     IN, IK = layout
     BK = IK * k_pack
@@ -172,24 +173,29 @@ def shuffle_weight(
     return x.contiguous()
 
 
-def assert_tl_matmul_correctness(M,
-                                 N,
-                                 K,
-                                 k_pack=1,
-                                 a_transposed=False,
-                                 b_transposed=True,
-                                 b_preshuffle=True):
+def assert_tl_matmul_correctness(M, N, K, k_pack=1, a_transposed=False, b_transposed=True, b_preshuffle=True):
     in_dtype = T.float8_e4m3fnuz
     out_dtype = T.float32
     accum_dtype = T.float32
-    kernel = tl_matmul(M, N, K, k_pack=k_pack, in_dtype=in_dtype, out_dtype=out_dtype, accum_dtype=accum_dtype, a_transposed=a_transposed, b_transposed=b_transposed, b_preshuffle=b_preshuffle)
-    
+    kernel = tl_matmul(
+        M,
+        N,
+        K,
+        k_pack=k_pack,
+        in_dtype=in_dtype,
+        out_dtype=out_dtype,
+        accum_dtype=accum_dtype,
+        a_transposed=a_transposed,
+        b_transposed=b_transposed,
+        b_preshuffle=b_preshuffle,
+    )
+
     src_code = kernel.get_kernel_source()
     # src_code is the generated cuda source
     assert src_code is not None
     A_shape = (K, M) if a_transposed else (M, K)
     B_shape = (N, K) if b_transposed else (K, N)
-    
+
     A = (torch.rand(A_shape, device="cuda", dtype=torch.float16) / 10).to(getattr(torch, in_dtype))
     B = (torch.rand(B_shape, device="cuda", dtype=torch.float16) / 10).to(getattr(torch, in_dtype))
 
@@ -198,7 +204,10 @@ def assert_tl_matmul_correctness(M,
         B_preshuffle = shuffle_weight(B_preshuffle, k_pack=k_pack, is_transpose=b_transposed)
         C = kernel(A, B_preshuffle)
     else:
-        C = kernel(A, B,)
+        C = kernel(
+            A,
+            B,
+        )
 
     profiler = kernel.get_profiler()
     latency = profiler.do_bench()
@@ -209,16 +218,13 @@ def assert_tl_matmul_correctness(M,
 
     if a_transposed and b_transposed:
         # Get Reference Result
-        ref_c = torch.matmul(A.T.half(),
-                             B.T.half()).to(getattr(torch, out_dtype))
+        ref_c = torch.matmul(A.T.half(), B.T.half()).to(getattr(torch, out_dtype))
     elif a_transposed and not b_transposed:
         # Get Reference Result
-        ref_c = torch.matmul(A.T.half(),
-                             B.half()).to(getattr(torch, out_dtype))
+        ref_c = torch.matmul(A.T.half(), B.half()).to(getattr(torch, out_dtype))
     elif not a_transposed and b_transposed:
         # Get Reference Result
-        ref_c = torch.matmul(A.half(),
-                             B.T.half()).to(getattr(torch, out_dtype))
+        ref_c = torch.matmul(A.half(), B.T.half()).to(getattr(torch, out_dtype))
     else:
         # Get Reference Result
         ref_c = torch.matmul(A.half(), B.half()).to(getattr(torch, out_dtype))
@@ -227,13 +233,9 @@ def assert_tl_matmul_correctness(M,
 
 
 def test_assert_tl_matmul():
-    assert_tl_matmul_correctness(
-        512, 512, 512, k_pack=1, b_transposed=True, b_preshuffle=True)
-    assert_tl_matmul_correctness(
-        512, 512, 512, k_pack=2, b_transposed=True, b_preshuffle=True)
-    
+    assert_tl_matmul_correctness(512, 512, 512, k_pack=1, b_transposed=True, b_preshuffle=True)
+    assert_tl_matmul_correctness(512, 512, 512, k_pack=2, b_transposed=True, b_preshuffle=True)
 
 
 if __name__ == "__main__":
     test_assert_tl_matmul()
-
