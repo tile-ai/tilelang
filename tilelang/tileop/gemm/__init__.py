4b1783e4d7b37efcd4f22a926f7d8452eaea1b9e
Rachmanino <18805904201@163.com>
2026-02-07 18:02:38 +0800

    lint

--- tilelang/tileop/gemm/__init__.py
+++ tilelang/tileop/gemm/__init__.py
@@ -12,7 +12,6 @@ from .gemm_wgmma import GemmWGMMA
 from .gemm_tcgen05 import GemmTCGEN5
 from .gemm_mfma import GemmMFMA
 from .gemm_cutedsl import GemmCuTeDSL
-from tilelang import _ffi_api
 from tilelang.utils.target import target_is_volta
 from tilelang.jit.adapter.utils import is_cutedsl_target
 
