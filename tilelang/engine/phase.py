fe8ceb27cb5d9784395b77375d76c79d05d2b3c6
LeiWang1999 <leiwang1999@outlook.com>
2026-04-03 16:43:18 +0800

    implement descriptor reuse pass

--- tilelang/engine/phase.py
+++ tilelang/engine/phase.py
@@ -238,6 +238,7 @@ def OptimizeForTarget(mod: IRModule, target: Target) -> IRModule:
         mod = tilelang.transform.FuseMBarrierArriveExpectTx()(mod)
     mod = tilelang.transform.HoistGlobalBufferAllocations()(mod)
     mod = tilelang.transform.LowerOpaqueBlock()(mod)
+    mod = tilelang.transform.ReuseLocalDescriptorAllocations()(mod)
     if is_hopper(target):
         mod = tilelang.transform.RewriteWgmmaSync()(mod)
     mod = tilelang.transform.Simplify()(mod)
