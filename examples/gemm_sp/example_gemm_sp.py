1c90464e0096e93bf9adbd7586be0216a2fc32a6
botbw <wang1570@e.ntu.edu.sg>
2026-05-11 13:12:54 +0000

    Update example

--- examples/gemm_sp/example_gemm_sp.py
+++ examples/gemm_sp/example_gemm_sp.py
@@ -8,21 +8,21 @@ from tilelang.profiler import do_bench
 
 import torch
 
-E_FACTOR = get_e_factor(T.float16, T.int16)
-
 
 @tilelang.jit(out_idx=[-1])
-def matmul_sp_fp16(M, N, K, accum_dtype, block_M, block_N, block_K, num_stages, thread_num, policy, enable_rasterization):
+def matmul_sp_fp16(M, N, K, accum_dtype, e_dtype, block_M, block_N, block_K, num_stages, thread_num, policy, enable_rasterization):
+    e_factor = get_e_factor(T.float16, e_dtype)
+
     @T.prim_func
     def gemm_sp_fp16(
         A_sparse: T.Tensor((M, K // 2), T.float16),
-        E: T.Tensor((M, K // E_FACTOR), T.int16),
+        E: T.Tensor((M, K // e_factor), e_dtype),
         B: T.Tensor((K, N), T.float16),
         C: T.Tensor((M, N), accum_dtype),
     ):
         with T.Kernel(T.ceildiv(N, block_N), T.ceildiv(M, block_M), threads=thread_num) as (bx, by):
             A_shared = T.alloc_shared((block_M, block_K // 2), T.float16)
-            E_shared = T.alloc_shared((block_M, block_K // E_FACTOR), T.int16)
+            E_shared = T.alloc_shared((block_M, block_K // e_factor), e_dtype)
             B_shared = T.alloc_shared((block_K, block_N), T.float16)
             C_shared = T.alloc_shared((block_M, block_N), accum_dtype)
             C_local = T.alloc_fragment((block_M, block_N), accum_dtype)
@@ -32,9 +32,9 @@ def matmul_sp_fp16(M, N, K, accum_dtype, block_M, block_N, block_K, num_stages, 
             T.use_swizzle(panel_size=10, enable=enable_rasterization)
             for k in T.Pipelined(T.ceildiv(K, block_K), num_stages=num_stages):
                 T.copy(A_sparse[by * block_M, k * block_K // 2], A_shared)
-                T.copy(E[by * block_M, k * block_K // E_FACTOR], E_shared)
+                T.copy(E[by * block_M, k * block_K // e_factor], E_shared)
                 T.copy(B[k * block_K, bx * block_N], B_shared)
-                T.gemm_sp(A_shared, E_shared, B_shared, C_local, False, False, policy=policy)
+                T.gemm_sp(A_shared, E_shared, B_shared, C_local, False, False, False, policy=policy)
 
             T.copy(C_local, C_shared)
             T.copy(C_shared, C[by * block_M, bx * block_N])
@@ -47,6 +47,7 @@ def main(
     N=1024,
     K=1024,
     accum_dtype=T.float,
+    e_dtype=T.int16,
     block_M=128,
     block_N=128,
     block_K=64,
@@ -55,12 +56,12 @@ def main(
     policy=T.GemmWarpPolicy.Square,
     enable_rasterization=True,
 ):
-    kernel = matmul_sp_fp16(M, N, K, accum_dtype, block_M, block_N, block_K, num_stages, thread_num, policy, enable_rasterization)
+    kernel = matmul_sp_fp16(M, N, K, accum_dtype, e_dtype, block_M, block_N, block_K, num_stages, thread_num, policy, enable_rasterization)
 
     a = randn_semi_sparse(M, K, device="cuda", dtype=torch.half)
     b = torch.randn(K, N, device="cuda", dtype=torch.half)
 
-    a_sparse, e = compress(a)
+    a_sparse, e = compress(a, meta_dtype=e_dtype.as_torch())
     c = kernel(a_sparse, e, b)
 
     ref_c = a @ b
@@ -84,18 +85,25 @@ if __name__ == "__main__":
     parser.add_argument("--m", type=int, default=16384, help="Matrix dimension M")
     parser.add_argument("--n", type=int, default=16384, help="Matrix dimension N")
     parser.add_argument("--k", type=int, default=16384, help="Matrix dimension K")
-    parser.add_argument("--accum_dtype", type=str, default=T.float, choices=[T.float, T.float16], help="Accumulation datatype")
+    parser.add_argument(
+        "--e_dtype",
+        default=T.int16,
+        choices=[T.int8, T.int16, T.int32],
+        help="Data type for metadata E, which controls the sparsity pattern. Note that int8 and int32 are only supported on sm90+",
+    )
+    parser.add_argument("--accum_dtype", default=T.float, choices=[T.float, T.float16], help="Accumulation datatype")
     parser.add_argument("--block_M", type=int, default=128)
-    parser.add_argument("--block_N", type=int, default=128)
-    parser.add_argument("--block_K", type=int, default=64)
+    parser.add_argument("--block_N", type=int, default=256)
+    parser.add_argument("--block_K", type=int, default=128)
     parser.add_argument("--num_stages", type=int, default=2)
-    parser.add_argument("--thread_num", type=int, default=128)
+    parser.add_argument("--thread_num", type=int, default=256)
     args = parser.parse_args()
     main(
         M=args.m,
         N=args.n,
         K=args.k,
         accum_dtype=args.accum_dtype,
+        e_dtype=args.e_dtype,
         block_M=args.block_M,
         block_N=args.block_N,
         block_K=args.block_K,
