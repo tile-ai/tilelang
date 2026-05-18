41467442e018c3d3fe3e97349ef1c0a037301573
Rachmanino <18805904201@163.com>
2026-05-18 15:32:50 +0800

    Remove dequant GEMM regression test

--- examples/dequantize_gemm/test_example_dequantize_gemm.py
+++ examples/dequantize_gemm/test_example_dequantize_gemm.py
@@ -1,7 +1,4 @@
-import tilelang
-import tilelang.language as T
 import tilelang.testing
-from tilelang import tvm
 
 import example_dequant_gemv_fp16xint4
 import example_dequant_gemm_fp4_hopper
@@ -24,29 +21,6 @@ def test_example_dequant_gemm_fp4_hopper():
 @tilelang.testing.requires_cuda_compute_version_eq(9, 0)
 def test_example_dequant_gemm_bf16_mxfp4_hopper():
     example_dequant_gemm_bf16_mxfp4_hopper.main()
-
-
-def test_example_dequant_gemm_bf16_mxfp4_hopper_ws_bias_lower():
-    with tvm.target.Target("cuda -arch=sm_90"):
-        func = example_dequant_gemm_bf16_mxfp4_hopper.matmul.jit_impl.get_tir(
-            256,
-            256,
-            256,
-            T.bfloat16,
-            T.bfloat16,
-            T.float32,
-            num_bits=4,
-            scale_size=32,
-            block_M=256,
-            block_N=128,
-            block_K=128,
-            num_stages=2,
-            threads=256,
-            split=1,
-            fast_dequant=True,
-            with_bias=True,
-        )
-        tilelang.lower(func, target="cuda -arch=sm_90", enable_device_compile=False)
 
 
 @tilelang.testing.requires_cuda
