c08cc1514ae81f3008e3c1c27644847451f0b19c
SiriusNEO <chaofan@deepseek.com>
2026-06-08 16:04:45 +0800

    [Backend] Remove JIT execution backend shim

--- tilelang/jit/execution_backend.py
+++
@@ -1,9 +0,0 @@
-from tilelang.backend.execution_backend import (  # noqa: F401
-    ExecutionBackendSpec,
-    allowed_backends_for_target,
-    canonicalize_execution_backend,
-    register_execution_backend,
-    register_lazy_execution_backends,
-    resolve_execution_backend,
-    resolve_execution_backend_spec,
-)
