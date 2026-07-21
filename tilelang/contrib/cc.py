cd43922da64c74255a364e390efc288475615198
Zheng Yu <17133261+cla7aye15I4nd@users.noreply.github.com>
2026-07-21 00:04:55 -0700

    Keep cross-compiler options isolated per build

--- tilelang/contrib/cc.py
+++ tilelang/contrib/cc.py
@@ -350,7 +350,7 @@ def cross_compiler(compile_func, options=None, output_format=None, get_target_tr
         compile_func = create_shared
 
     def _fcompile(outputs, objects, options=None):
-        all_options = base_options
+        all_options = list(base_options)
         if options is not None:
             all_options += options
         compile_func(outputs, objects + add_files, options=all_options, **kwargs)
