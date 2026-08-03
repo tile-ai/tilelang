fcb30c1776fe9e0f7001361e76aa5075441c952d
guanjiawei <128683929+skyguan92@users.noreply.github.com>
2026-08-03 10:24:58 +0800

    [ROCm] Enable software FP4 activation quantization
    Signed-off-by: guanjiawei <128683929+skyguan92@users.noreply.github.com>

--- src/rocm/codegen/codegen_hip.h
+++ src/rocm/codegen/codegen_hip.h
@@ -51,6 +51,8 @@ public:
   void VisitExpr_(const CallNode *op, std::ostream &os) final;
   void VisitExpr_(const CastNode *op, std::ostream &os) final;
   void VisitExpr_(const ShuffleNode *op, std::ostream &os) final; // NOLINT(*)
+  void VisitExpr_(const BufferLoadNode *op,
+                  std::ostream &os) final; // NOLINT(*)
   void VisitStmt_(const AllocBufferNode *op) final;
   void VisitStmt_(const AttrStmtNode *op) final;
   void VisitStmt_(const BufferStoreNode *op) final;
@@ -84,9 +86,9 @@ private:
   bool need_wmma_h_{false};
   // whether need fp8.h
   bool enable_fp8_{false};
-  // whether need hip_fp4.h (gfx950 only)
+  // whether need the software FP4 helpers in hip_fp4.h
   bool enable_fp4_{false};
-  // Map from FP4 buffer Var to packed buffer variable name (gfx950)
+  // Map from an FP4 buffer Var to its packed buffer variable name
   std::unordered_map<Var, std::string> fp4_packed_buffers_;
   // The size of the barrier array in shared memory
   int barrier_count_ = -1;
