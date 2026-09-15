ed29103eb5ed45912a9cdf7fb5e7d78829a1b8c4
sepcnt <30561671+sepcnt@users.noreply.github.com>
2026-09-15 14:31:10 +0800

    [JIT] Add missing uint64 argument type mappings

--- tilelang/jit/adapter/cython/cython_wrapper.pyx
+++ tilelang/jit/adapter/cython/cython_wrapper.pyx
@@ -236,6 +236,7 @@ cdef class CythonKernelWrapper:
             torch.int16: ctypes.c_int16,
             torch.int32: ctypes.c_int32,
             torch.int64: ctypes.c_int64,
+            torch.uint64: ctypes.c_uint64,
             torch.bool: ctypes.c_bool,
         }
 
